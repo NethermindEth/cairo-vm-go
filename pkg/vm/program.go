db56d1649c5175cae7aa0028003f90cabc47f305
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2023-07-30 18:54:19 +0300

    Cleanup Program parsing logic
    felt.Felt can be directly unmarshaled from JSON

--- pkg/vm/program.go
+++ pkg/vm/program.go
@@ -1,26 +1,16 @@
 package vm
 
 import (
-	"encoding/binary"
-	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"os"
 
 	f "github.com/NethermindEth/juno/core/felt"
-	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
-	"golang.org/x/exp/constraints"
 )
 
 type Program struct {
-	Prime    *f.Felt
-	Bytecode *[]string
-}
-
-type JSONContent struct {
-	Prime           string
-	CompilerVersion string
-	Bytecode        []string
+	// Prime is fixed to be 0x800000000000011000000000000000000000000000000000000000000000001 and wont fit in a f.Felt
+	Bytecode        []f.Felt `json:"bytecode"`
+	CompilerVersion string   `json:"compiler_version"`
 	// todo(rodro): Add remaining Json fields
 	// Hints
 	// EntryPointsByType
@@ -33,49 +23,10 @@ func ProgramFromFile(pathToFile string) (*Program, error) {
 	if error != nil {
 		return nil, error
 	}
-	return ProgramFromBytes(content)
+	return ProgramFromJSON(content)
 }
 
-func ProgramFromBytes(content []byte) (*Program, error) {
-	var jsonContent JSONContent
-	err := json.Unmarshal(content, &jsonContent)
-	if err != nil {
-		return nil, err
-	}
-
-    prime, err := parsePrime(jsonContent.Prime)
-	if err != nil {
-		return nil, err
-	}
-
-    return &Program{Prime: prime, Bytecode: &jsonContent.Bytecode}, nil
-}
-
-func parsePrime(prime string) (*f.Felt, error) {
-
-	decodedPrime, err := hex.DecodeString(prime)
-	if err != nil {
-		return &f.Zero, err
-	}
-
-    wordAmount := len(decodedPrime) / 4
-
-	words := make([]uint64, wordAmount)
-	for i := 0; i < wordAmount; i++ {
-		word_i, n := binary.Uvarint(decodedPrime[i*64 : max((i+1)*64, len(decodedPrime))])
-		if n <= 0 {
-			return &f.Zero, fmt.Errorf("Prime parsing: invalid number: %d", n)
-		}
-		words[i] = word_i
-	}
-
-    x := fp.Element(words)
-    return f.NewFelt(&x), nil
-}
-
-func max[T constraints.Ordered](x, y T) T {
-    if x > y {
-        return x
-    }
-    return y
+func ProgramFromJSON(content json.RawMessage) (*Program, error) {
+	var program Program
+	return &program, json.Unmarshal(content, &program)
 }
