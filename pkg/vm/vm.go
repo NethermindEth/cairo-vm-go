4a304f06acf4c08f220d26b277696f2798449718
Rodrigo <rodrodpino@gmail.com>
2023-08-03 15:04:35 -0400

    Add initial impl of Memory Segment

--- pkg/vm/vm.go
+++ pkg/vm/vm.go
@@ -1,13 +1,41 @@
 package vm
 
+import (
+	f "github.com/NethermindEth/juno/core/felt"
+)
+
+type Context struct {
+	Fp uint
+	Ap uint
+	Pc uint
+}
+
+type VirtualMachineConfig struct {
+	Trace bool
+	// Todo(rodro): Update this property to include all builtins
+	Builtins bool
+}
+
 type VirtualMachine struct {
-	// todo(rodro): define state fields
+	Context              Context
+	MemorySegmentManager MemorySegmentManager
+	Config               VirtualMachineConfig
 }
 
-func NewVirtualMachine(program *Program) *VirtualMachine {
-	return &VirtualMachine{}
+// NewVirtualMachine creates a VM from the program bytecode using a specified config.
+func NewVirtualMachine(programBytecode *[]f.Felt, config VirtualMachineConfig) *VirtualMachine {
+	return &VirtualMachine{
+		Context{Fp: 0, Ap: 0, Pc: 0},
+		CreateMemorySegmentManager(programBytecode),
+		config,
+	}
 }
 
-func (vm *VirtualMachine) Run() error {
-    return nil
+// RunInstructionFrom executes the program starting at the specified Program Counter.
+func (vm *VirtualMachine) RunStep() error {
+	return nil
+}
+
+func (vm *VirtualMachine) RunInstruction() error {
+	return nil
 }
