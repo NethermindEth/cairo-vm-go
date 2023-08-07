3ea7ded75e871436bc6f7b54714f8d934d11265d
Rodrigo <rodrodpino@gmail.com>
2023-08-07 17:35:07 -0400

    Revamped relocatable code

--- pkg/vm/memory/relocatable.go
+++ pkg/vm/memory/relocatable.go
@@ -6,89 +6,162 @@ import (
 	f "github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
 )
 
-// The default segment that represents ordinary felts.
-const feltSegment = 0
-
 // Represents a Memory Address of the Cairo VM. Because memory is split between different segments
 // during execution, addresses has two locators: the segment they belong to and the location
 // inside that segment
 type MemoryAddress struct {
 	SegmentIndex uint64
-	Offset       f.Element
+	Offset       *f.Element
 }
 
 // Creates a new memory address
 func CreateMemoryAddress(segment uint64, offset *f.Element) *MemoryAddress {
-	return &MemoryAddress{SegmentIndex: segment, Offset: *offset}
+	return &MemoryAddress{SegmentIndex: segment, Offset: offset}
 }
 
-// Adds two relocatables if the second one is a Felt (relocatable of the form 0:off).
-func (r *MemoryAddress) Add(r1 *MemoryAddress, r2 *MemoryAddress) (*MemoryAddress, error) {
-	if r2.SegmentIndex != feltSegment {
-		return nil, fmt.Errorf("cannot add two relocatables")
+func (address MemoryAddress) String() string {
+	return fmt.Sprintf("Memory Address: segment: %d, offset: %s", address.SegmentIndex, address.Offset.Text(10))
+}
+
+// Wraps all posible types that can be stored in a Memory cell,
+//
+//   - either a Felt value (an `f.Element`),
+//   - or a pointer to another Memory Cell (a `MemoryAddress`)
+type MemoryValue struct {
+	// When isAddress is true, this indicates the segment of
+	// the memory address
+	segmentIndex uint64
+	// This represents the offset if it is an address or the
+	// value if it is a field element
+	value     *f.Element
+	isAddress bool
+}
+
+func EmptyMemoryValue() *MemoryValue {
+	return &MemoryValue{
+		value: new(f.Element),
+	}
+}
+
+func (mv *MemoryValue) ToMemoryAddress() (*MemoryAddress, error) {
+	if !mv.isAddress {
+		return nil, fmt.Errorf("error trying to read a memory address as field element")
+	}
+	return &MemoryAddress{
+		SegmentIndex: mv.segmentIndex,
+		Offset:       mv.value,
+	}, nil
+}
+
+func (mv *MemoryValue) ToFieldElement() (*f.Element, error) {
+	if mv.isAddress {
+		return nil, fmt.Errorf("error trying to read a field element as a memory address")
+	}
+	return mv.value, nil
+}
+
+func MemoryValueFromMemoryAddress(address *MemoryAddress) *MemoryValue {
+	return &MemoryValue{
+		segmentIndex: address.SegmentIndex,
+		value:        address.Offset,
+		isAddress:    true,
+	}
+}
+
+func MemoryValueFromFieldElement(felt *f.Element) *MemoryValue {
+	return &MemoryValue{
+		value:     felt,
+		isAddress: false,
+	}
+}
+
+func MemoryValueFromUint64(v uint64) *MemoryValue {
+	newElement := f.NewElement(v)
+	return &MemoryValue{
+		value: &newElement,
+	}
+}
+
+// Adds two memory value is the second one is a Felt
+func (memVal *MemoryValue) Add(lhs *MemoryValue, rhs *MemoryValue) (*MemoryValue, error) {
+	if rhs.isAddress {
+		return nil, fmt.Errorf("cannot add two memory addresses")
 	}
 
-	r.SegmentIndex = r1.SegmentIndex
-	r.Offset.Add(&r1.Offset, &r2.Offset)
+	fmt.Println(memVal)
+	fmt.Println("x0")
+	fmt.Println(lhs)
+	fmt.Println("x1")
+	fmt.Println(rhs)
 
-	return r, nil
+	// todo(rodro): is lhs always a memory address?
+	if lhs.isAddress {
+		memVal.isAddress = true
+		memVal.segmentIndex = lhs.segmentIndex
+	}
+	fmt.Println("x2", lhs.value, rhs.value)
+	memVal.value.Add(lhs.value, rhs.value)
+	fmt.Println("x3")
+
+	return memVal, nil
 }
 
 // Subs two relocatables if they're in the same segment or the rhs is a Felt.
-func (r *MemoryAddress) Sub(r1 *MemoryAddress, r2 *MemoryAddress) (*MemoryAddress, error) {
-	if r2.SegmentIndex == feltSegment {
-		r.SegmentIndex = r1.SegmentIndex
-		r.Offset.Sub(&r1.Offset, &r2.Offset)
-		return r, nil
+func (memVal *MemoryValue) Sub(lhs *MemoryValue, rhs *MemoryValue) (*MemoryValue, error) {
+	if !rhs.isAddress {
+		// todo(rodro): is lhs always a memory address?
+		if lhs.isAddress {
+			memVal.isAddress = true
+			memVal.segmentIndex = lhs.segmentIndex
+		}
+		memVal.value.Sub(lhs.value, rhs.value)
+		return memVal, nil
 	}
 
-	if r1.SegmentIndex != r2.SegmentIndex {
-		return nil, fmt.Errorf("cannot subtract relocatables from different segments: %d != %d", r1.SegmentIndex, r2.SegmentIndex)
+	// todo(rodro): can lhs not be a memory address?
+	if !lhs.isAddress {
+		return nil, fmt.Errorf("sub not implemented for lhs as non address")
 	}
 
-	r.SegmentIndex = feltSegment
-	r.Offset.Sub(&r1.Offset, &r2.Offset)
+	if lhs.segmentIndex != rhs.segmentIndex {
+		return nil, fmt.Errorf("cannot subtract relocatables from different segments: %d != %d", lhs.segmentIndex, rhs.segmentIndex)
+	}
 
-	return r, nil
+	memVal.isAddress = true
+	memVal.segmentIndex = lhs.segmentIndex
+	memVal.value.Sub(lhs.value, rhs.value)
+
+	return memVal, nil
 }
 
-// String representation of a relocatable.
-func (r MemoryAddress) String() string {
-	return fmt.Sprintf("%d:%d", r.SegmentIndex, r.Offset)
+func (memVal MemoryValue) String() string {
+	if memVal.isAddress {
+		return MemoryAddress{
+			memVal.segmentIndex,
+			memVal.value,
+		}.String()
+	}
+	return memVal.value.String()
 }
 
+// Note: Commenting this function since relocation is possibly going to look
+// different.
 // Given a map of segment relocation, update a memory address location
-func (r *MemoryAddress) Relocate(r1 *MemoryAddress, segmentsOffsets *map[uint64]*MemoryAddress) (*MemoryAddress, error) {
-	if (*segmentsOffsets)[r1.SegmentIndex] == nil {
-		return nil, fmt.Errorf("missing segment %d relocation rule", r.SegmentIndex)
-	}
-
-	r, err := r.Add((*segmentsOffsets)[r1.SegmentIndex], &MemoryAddress{0, r1.Offset})
-
-	return r, err
-}
+//func (r *MemoryAddress) Relocate(r1 *MemoryAddress, segmentsOffsets *map[uint64]*MemoryAddress) (*MemoryAddress, error) {
+//	if (*segmentsOffsets)[r1.SegmentIndex] == nil {
+//		return nil, fmt.Errorf("missing segment %d relocation rule", r.SegmentIndex)
+//	}
+//
+//	r, err := r.Add((*segmentsOffsets)[r1.SegmentIndex], &MemoryAddress{0, r1.Offset})
+//
+//	return r, err
+//}
 
 // Turns a relocatable of the form 0:offset into offset.Uint64()
 // otherwise fails.
-func (r *MemoryAddress) Uint64() (uint64, error) {
-	if r.SegmentIndex == feltSegment {
-		return r.Offset.Uint64(), nil
+func (memVal *MemoryValue) Uint64() (uint64, error) {
+	if memVal.isAddress {
+		return 0, fmt.Errorf("cannot convert a memory address '%s' into uint64", *memVal)
 	}
-	return 0, fmt.Errorf("cannot convert a relocatable '%s' into uint64", *r)
-}
-
-// Sets a relocatable into the relocatable of the form 0:r.Offset.SetUint64(v).
-func (r *MemoryAddress) SetUint64(v uint64) *MemoryAddress {
-	r.SegmentIndex = feltSegment
-	r.Offset.SetUint64(v)
-
-	return r
-}
-
-// Sets a relocatable into the relocatable of the form 0:v.
-func (r *MemoryAddress) SetFelt(v *f.Element) *MemoryAddress {
-	r.SegmentIndex = feltSegment
-	r.Offset.Set(v)
-
-	return r
+	return memVal.value.Uint64(), nil
 }
--- pkg/vm/memory/relocatable_test.go
+++ pkg/vm/memory/relocatable_test.go
@@ -1,6 +1,7 @@
 package memory
 
 import (
+	"fmt"
 	"testing"
 
 	f "github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
@@ -8,105 +9,145 @@ import (
 )
 
 func TestAddFelt(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-	r2 := new(MemoryAddress).SetUint64(2)
-	expected := CreateMemoryAddress(2, new(f.Element).SetUint64(12))
+	memVal := EmptyMemoryValue()
+	lhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(10),
+	})
+	rhs := MemoryValueFromFieldElement(new(f.Element).SetUint64(2))
 
-	res, err := r.Add(r1, r2)
+	expected := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(12),
+	})
+	fmt.Println("a-1")
 
+	res, err := memVal.Add(lhs, rhs)
+
+	fmt.Println("a-2")
 	assert.NoError(t, err)
 
-	assert.Equal(t, r, res)
+	fmt.Println("a0")
+
+	assert.Equal(t, memVal, res)
+	fmt.Println("a1")
 	assert.Equal(t, *res, *expected)
+	fmt.Println("a2")
 }
 
 func TestAddRelocatable(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-	r2 := CreateMemoryAddress(2, new(f.Element).SetUint64(2))
+	memVal := EmptyMemoryValue()
+	lhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(10),
+	})
+	rhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(2),
+	})
+	memVal, err := memVal.Add(lhs, rhs)
 
-	r, err := r.Add(r1, r2)
-
-	assert.Nil(t, r)
+	assert.Nil(t, memVal)
 	assert.Error(t, err)
 }
 
 func TestSubFelt(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-	r2 := new(MemoryAddress).SetUint64(2)
-	expected := CreateMemoryAddress(2, new(f.Element).SetUint64(8))
+	memVal := EmptyMemoryValue()
+	lhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(10),
+	})
+	rhs := MemoryValueFromFieldElement(new(f.Element).SetUint64(2))
 
-	res, err := r.Sub(r1, r2)
+	expected := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(8),
+	})
+
+	res, err := memVal.Sub(lhs, rhs)
 
 	assert.NoError(t, err)
-
-	assert.Equal(t, r, res)
+	assert.Equal(t, memVal, res)
 	assert.Equal(t, *res, *expected)
 }
 
 func TestSubSameSegment(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-	r2 := CreateMemoryAddress(2, new(f.Element).SetUint64(2))
-	expected := new(MemoryAddress).SetUint64(8)
+	memVal := EmptyMemoryValue()
+	lhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(10),
+	})
+	rhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(2),
+	})
+	expected := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(8),
+	})
 
-	res, err := r.Sub(r1, r2)
+	res, err := memVal.Sub(lhs, rhs)
 
 	assert.NoError(t, err)
 
-	assert.Equal(t, r, res)
+	assert.Equal(t, memVal, res)
 	assert.Equal(t, *res, *expected)
 }
 
 func TestSubDifferentSegment(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-	r2 := CreateMemoryAddress(1, new(f.Element).SetUint64(2))
+	memVal := EmptyMemoryValue()
+	lhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 2,
+		Offset:       new(f.Element).SetUint64(10),
+	})
+	rhs := MemoryValueFromMemoryAddress(&MemoryAddress{
+		SegmentIndex: 5,
+		Offset:       new(f.Element).SetUint64(2),
+	})
 
-	r, err := r.Sub(r1, r2)
+	memVal, err := memVal.Sub(lhs, rhs)
 
-	assert.Nil(t, r)
+	assert.Nil(t, memVal)
 	assert.Error(t, err)
 }
 
-func TestRelocate1(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-	expected := new(MemoryAddress).SetUint64(52)
-
-	res, err := r.Relocate(r1, &map[uint64]*MemoryAddress{
-		2: new(MemoryAddress).SetUint64(42),
-	})
-
-	assert.NoError(t, err)
-	assert.Equal(t, res, r)
-	assert.Equal(t, *res, *expected)
-}
-
-func TestRelocate2(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-	expected := CreateMemoryAddress(10, new(f.Element).SetUint64(11))
-
-	res, err := r.Relocate(r1, &map[uint64]*MemoryAddress{
-		2: CreateMemoryAddress(10, new(f.Element).SetUint64(1)),
-	})
-
-	assert.NoError(t, err)
-	assert.Equal(t, res, r)
-	assert.Equal(t, *res, *expected)
-}
-
-func TestRelocateMissingRule(t *testing.T) {
-	r := new(MemoryAddress)
-	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
-
-	res, err := r.Relocate(r1, &map[uint64]*MemoryAddress{
-		3: CreateMemoryAddress(10, new(f.Element).SetUint64(1)),
-	})
-
-	assert.Error(t, err)
-	assert.Nil(t, res)
-}
+// Note: Leaving relocation logic for later
+//func TestRelocate1(t *testing.T) {
+//	r := new(MemoryAddress)
+//	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
+//	expected := new(MemoryAddress).SetUint64(52)
+//
+//	res, err := r.Relocate(r1, &map[uint64]*MemoryAddress{
+//		2: new(MemoryAddress).SetUint64(42),
+//	})
+//
+//	assert.NoError(t, err)
+//	assert.Equal(t, res, r)
+//	assert.Equal(t, *res, *expected)
+//}
+//
+//func TestRelocate2(t *testing.T) {
+//	r := new(MemoryAddress)
+//	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
+//	expected := CreateMemoryAddress(10, new(f.Element).SetUint64(11))
+//
+//	res, err := r.Relocate(r1, &map[uint64]*MemoryAddress{
+//		2: CreateMemoryAddress(10, new(f.Element).SetUint64(1)),
+//	})
+//
+//	assert.NoError(t, err)
+//	assert.Equal(t, res, r)
+//	assert.Equal(t, *res, *expected)
+//}
+//
+//func TestRelocateMissingRule(t *testing.T) {
+//	r := new(MemoryAddress)
+//	r1 := CreateMemoryAddress(2, new(f.Element).SetUint64(10))
+//
+//	res, err := r.Relocate(r1, &map[uint64]*MemoryAddress{
+//		3: CreateMemoryAddress(10, new(f.Element).SetUint64(1)),
+//	})
+//
+//	assert.Error(t, err)
+//	assert.Nil(t, res)
+//}
