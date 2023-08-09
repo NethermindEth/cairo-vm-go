8f2f398294069d9cb604347ed201a343fcb1b4e1
Rodrigo <rodrodpino@gmail.com>
2023-08-09 19:11:36 -0400

    Add function descriptions

--- pkg/vm/memory/memory.go
+++ pkg/vm/memory/memory.go
@@ -33,18 +33,21 @@ func EmptySegmentWithLength(length int) Segment {
 		Data: make([]Cell, length),
 	}
 }
-func (segment *Segment) Write(index uint64, value *MemoryValue) error {
-	cell := segment.Data[index]
+
+// Writes a new memory value to a specified offset, errors in case of overwriting an existing cell
+func (segment *Segment) Write(offset uint64, value *MemoryValue) error {
+	cell := segment.Data[offset]
 	if cell.Accessed {
-		return fmt.Errorf("rewriting cell at %d, old value: %d new value: %d", index, &cell.Value, &value)
+		return fmt.Errorf("rewriting cell at %d, old value: %d new value: %d", offset, &cell.Value, &value)
 	}
 	cell.Accessed = true
 	cell.Value = value
 	return nil
 }
 
-func (segment *Segment) Read(index uint64) *MemoryValue {
-	cell := segment.Data[index]
+// Reads a memory value from a specified offset at the segment
+func (segment *Segment) Read(offset uint64) *MemoryValue {
+	cell := segment.Data[offset]
 	cell.Accessed = true
 	return cell.Value
 }
@@ -63,6 +66,7 @@ func InitializeEmptyMemory() *Memory {
 	}
 }
 
+// Allocates a new segment providing its initial data and returns its index
 func (memory *Memory) AllocateSegment(data *[]f.Element) (int, error) {
 	newSegment := EmptySegmentWithLength(len(*data))
 	for i := range *data {
@@ -76,12 +80,14 @@ func (memory *Memory) AllocateSegment(data *[]f.Element) (int, error) {
 	return len(memory.Segments), nil
 }
 
-// Allocates a new segment and returns its index
+// Allocates an empty segment and returns its index
 func (memory *Memory) AllocateEmptySegment() int {
 	memory.Segments = append(memory.Segments, EmptySegment())
 	return len(memory.Segments) - 1
 }
 
+// Writes to a memory address a new memory value. Errors if writing to an unallocated
+// space or if rewriting a specific cell
 func (memory *Memory) Write(address *MemoryAddress, value *MemoryValue) error {
 	if address.SegmentIndex > uint64(len(memory.Segments)) {
 		return fmt.Errorf("writing to unallocated segment %d", address.SegmentIndex)
@@ -90,6 +96,9 @@ func (memory *Memory) Write(address *MemoryAddress, value *MemoryValue) error {
 	return memory.Segments[address.SegmentIndex].Write(address.Offset, value)
 }
 
+// Reads a memory value from a memory address. Errors if reading from an unallocated
+// space. If reading a cell which hasn't been accesed before, it is initalized with
+// its default zero value
 func (memory *Memory) Read(address *MemoryAddress) (*MemoryValue, error) {
 	if address.SegmentIndex > uint64(len(memory.Segments)) {
 		return nil, fmt.Errorf("reading from unallocated segment %d", address.SegmentIndex)
