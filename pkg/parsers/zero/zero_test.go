2a20a86615b0c1c62c2be9d99584cdc830dbe823
Rodrigo <rodrodpino@gmail.com>
2023-09-13 17:29:00 -0400

    Update parsing tests

--- pkg/parsers/zero/zero_test.go
+++ pkg/parsers/zero/zero_test.go
@@ -1,105 +1,416 @@
 package zero
 
 import (
-	"encoding/json"
+	starknetParser "github.com/NethermindEth/cairo-vm-go/pkg/parsers/starknet"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestZeroParse(t *testing.T) {
-	tests := []struct {
-		name     string
-		jsonData string
-		expected ZeroProgram
-	}{
-		{
-			name: "Attributes field",
-			jsonData: `
-				{
-					"attributes": ["attr1", "attr2"]
-				}`,
-			expected: ZeroProgram{
-				Attributes: []string{"attr1", "attr2"},
+func TestPrime(t *testing.T) {
+	content := []byte(`
+        {
+            "prime": "191"
+        } 
+    `)
+
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(t,
+		&ZeroProgram{
+			Prime: "191",
+		},
+		zeroProgram,
+	)
+}
+
+func TestData(t *testing.T) {
+	content := []byte(`
+        {
+            "data": [
+                "0x00000002",
+                "0x00000003",
+                "0x00000005",
+                "0x00000007"
+            ]
+        }
+    `)
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(t,
+		&ZeroProgram{
+			Data: []string{
+				"0x00000002",
+				"0x00000003",
+				"0x00000005",
+				"0x00000007",
 			},
 		},
-		{
-			name: "Builtins field",
-			jsonData: `
-				{
-					"builtins": {"builtin1": 1, "builtin2": 2}
-				}`,
-			expected: ZeroProgram{
-				Builtins: map[string]int64{"builtin1": 1, "builtin2": 2},
+		zeroProgram,
+	)
+}
+
+func TestBuiltins(t *testing.T) {
+	content := []byte(`
+        {
+            "builtins": [
+                "output",
+                "range_check",
+                "bitwise"
+            ]     
+        } 
+    `)
+
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(t,
+		&ZeroProgram{
+			Builtins: []starknetParser.Builtin{
+				starknetParser.Output,
+				starknetParser.RangeCheck,
+				starknetParser.Bitwise,
 			},
 		},
-		{
-			name: "Code field",
-			jsonData: `
-				{
-					"code": "some code sample"
-				}`,
-			expected: ZeroProgram{
-				Code: "some code sample",
-			},
-		},
-		{
-			name: "Version field",
-			jsonData: `
-				{
-					"version": "0.12.2"
-				}`,
-			expected: ZeroProgram{
-				Version: "0.12.2",
-			},
-		},
-		{
-			name: "Prime field",
-			jsonData: `
-				{
-					"prime": "0x800000000000011000000000000000000000000000000000000000000000001"
-				}`,
-			expected: ZeroProgram{
-				Prime: "0x800000000000011000000000000000000000000000000000000000000000001",
-			},
-		},
-		{
-			name: "Hints field",
-			jsonData: `
-				{
-					"hints": {
-						"0": [
-							{
-								"accessible_scopes": [
-									"starkware.cairo.common.math",
-									"starkware.cairo.common.math.assert_not_equal"
-								],
-								"code": "from starkware.cairo.lang.vm.relocatable import RelocatableValue\nboth_ints = isinstance(ids.a, int) and isinstance(ids.b, int)\nboth_relocatable = (\n    isinstance(ids.a, RelocatableValue) and isinstance(ids.b, RelocatableValue) and\n    ids.a.segment_index == ids.b.segment_index)\nassert both_ints or both_relocatable, \\\n    f'assert_not_equal failed: non-comparable values: {ids.a}, {ids.b}.'\nassert (ids.a - ids.b) % PRIME != 0, f'assert_not_equal failed: {ids.a} = {ids.b}.'"
-							}
-						]
-					}
-				}`,
-			expected: ZeroProgram{
-				Hints: map[string][]Hint{
-					"0": {{
-						AccessibleScopes: []string{"starkware.cairo.common.math", "starkware.cairo.common.math.assert_not_equal"},
-						Code:             "from starkware.cairo.lang.vm.relocatable import RelocatableValue\nboth_ints = isinstance(ids.a, int) and isinstance(ids.b, int)\nboth_relocatable = (\n    isinstance(ids.a, RelocatableValue) and isinstance(ids.b, RelocatableValue) and\n    ids.a.segment_index == ids.b.segment_index)\nassert both_ints or both_relocatable, \\\n    f'assert_not_equal failed: non-comparable values: {ids.a}, {ids.b}.'\nassert (ids.a - ids.b) % PRIME != 0, f'assert_not_equal failed: {ids.a} = {ids.b}.'",
-					}},
+		zeroProgram,
+	)
+}
+
+func TestHints(t *testing.T) {
+	content := []byte(`
+        {
+            "hints": {
+                "6": [
+                    {
+                        "accessible_scopes": [
+                            "starkware.cairo.common.alloc",
+                            "starkware.cairo.common.alloc.alloc"
+                        ],
+                        "code": "memory[ap] = segments.add()",
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 2,
+                                "offset": 0
+                            },
+                            "reference_ids": {
+                                "starkware.cairo.common.math.assert_nn.a": 9,
+                                "starkware.cairo.common.math.assert_nn.range_check_ptr": 10
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    `)
+
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(t,
+		&ZeroProgram{
+			Hints: map[string][]Hint{
+				"6": {
+					{
+						AccessibleScopes: []string{
+							"starkware.cairo.common.alloc",
+							"starkware.cairo.common.alloc.alloc",
+						},
+						Code: "memory[ap] = segments.add()",
+						FlowTrackingData: FlowTrackingData{
+							ApTracking: ApTracking{
+								Group:  2,
+								Offset: 0,
+							},
+							ReferenceIds: map[string]uint64{
+								"starkware.cairo.common.math.assert_nn.a":               9,
+								"starkware.cairo.common.math.assert_nn.range_check_ptr": 10,
+							},
+						},
+					},
 				},
 			},
 		},
-		// TODO: Add debug-info test case
-	}
+		zeroProgram,
+	)
+}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			var z ZeroProgram
-			err := json.Unmarshal([]byte(tt.jsonData), &z)
-			if err != nil {
-				t.Errorf("Failed to unmarshal JSON: %s", err)
-			}
+func TestCompilerVersion(t *testing.T) {
+	content := []byte(`
+        {
+            "prime": "0x05"
+        }
+    `)
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
 
-			assert.Equal(t, tt.expected, z, "Field does not match")
-		})
-	}
+	require.Equal(
+		t,
+		&ZeroProgram{
+			Prime: "0x05",
+		},
+		zeroProgram,
+	)
+}
+
+func TestMainScope(t *testing.T) {
+	content := []byte(`
+        {
+            "main_scope": "__main__"
+        }
+    `)
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(
+		t,
+		&ZeroProgram{
+			MainScope: "__main__",
+		},
+		zeroProgram,
+	)
+}
+
+func TestIdentifiers(t *testing.T) {
+	content := []byte(`
+        {
+            "identifiers": {
+                "__main__.BitwiseBuiltin": {
+                    "destination": "starkware.cairo.common.cairo_builtins.BitwiseBuiltin",
+                    "type": "alias"
+                },
+                "__main__.fill_array.Args": {
+                    "full_name": "__main__.fill_array.Args",
+                    "members": {
+                        "array": {
+                            "cairo_type": "felt*",
+                            "offset": 0
+                        }
+                    },
+                    "size": 1,
+                    "type": "struct"
+                },
+                "__main__.fill_array.__temp18": {
+                    "cairo_type": "felt",
+                    "full_name": "__main__.fill_array.__temp18",
+                    "references": [
+                        {
+                            "ap_tracking_data": {
+                                "group": 26,
+                                "offset": 1
+                            },
+                            "pc": 312,
+                            "value": "[cast(ap + (-1), felt*)]"
+                        }
+                    ],
+                    "type": "reference"
+                }
+            }
+        }
+    `)
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(
+		t,
+		&ZeroProgram{
+			Identifiers: map[string]any{
+				"__main__.BitwiseBuiltin": map[string]any{
+					"destination": "starkware.cairo.common.cairo_builtins.BitwiseBuiltin",
+					"type":        "alias",
+				},
+				"__main__.fill_array.Args": map[string]any{
+					"full_name": "__main__.fill_array.Args",
+					"members": map[string]any{
+						"array": map[string]any{
+							"cairo_type": "felt*",
+							"offset":     float64(0),
+						},
+					},
+					"size": float64(1),
+					"type": "struct",
+				},
+				"__main__.fill_array.__temp18": map[string]any{
+					"cairo_type": "felt",
+					"full_name":  "__main__.fill_array.__temp18",
+					"references": []any{
+						map[string]any{
+							"ap_tracking_data": map[string]any{
+								"group":  float64(26),
+								"offset": float64(1),
+							},
+							"pc":    float64(312),
+							"value": "[cast(ap + (-1), felt*)]",
+						},
+					},
+					"type": "reference",
+				},
+			},
+		},
+		zeroProgram,
+	)
+}
+
+func TestAtributes(t *testing.T) {
+	content := []byte(`
+        {
+            "attributes": [
+                {
+                    "start_pc": 13,
+                    "end_pc": 17,
+                    "flow_tracking_data": {
+                        "ap_tracking": {
+                            "group": 2,
+                            "offset": 0
+                        },
+                        "reference_ids": {
+                            "ref_1": 1,
+                            "ref_2": 2
+                        }
+                    },
+                    "accessible_scopes": ["scope1", "scope2"]
+                }
+            ]
+        }
+    `)
+
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(
+		t,
+		&ZeroProgram{
+			Attributes: []AttributeScope{
+				{
+					StartPc: 13,
+					EndPc:   17,
+					FlowTrackingData: FlowTrackingData{
+						ApTracking: ApTracking{
+							Group:  2,
+							Offset: 0,
+						},
+						ReferenceIds: map[string]uint64{
+							"ref_1": 1,
+							"ref_2": 2,
+						},
+					},
+					AccessibleScopes: []string{
+						"scope1",
+						"scope2",
+					},
+				},
+			},
+		},
+		zeroProgram,
+	)
+}
+
+func TestDebugInfo(t *testing.T) {
+	content := []byte(`
+        {
+            "debug_info": {
+                "file_contents": {
+                    "key1": "value1"
+                },
+                "instruction_locations": {
+                    "7": {
+                        "accessible_scopes": [
+                            "starkware.cairo.lang.compiler.lib.registers",
+                            "starkware.cairo.lang.compiler.lib.registers.get_fp_and_pc"
+                        ],
+                        "flow_tracking_data": {
+                            "ap_tracking": {
+                                "group": 0,
+                                "offset": 0
+                            },
+                            "reference_ids": {
+                                "starkware.cairo.lang.compiler.lib.registers.get_ap.fp_val": 0,
+                                "starkware.cairo.lang.compiler.lib.registers.get_ap.pc_val": 1
+                            }
+                        },
+                        "hints": [
+                            {
+                                "location": {
+                                    "end_col": 38,
+                                    "end_line": 3,
+                                    "input_file": {
+                                        "filename": "some/path"
+                                    },
+                                    "start_col": 5,
+                                    "start_line": 3
+                                },
+                                "n_prefix_newlines": 0
+                            }
+                        ],
+                        "inst": {
+                            "end_col": 73,
+                            "end_line": 7,
+                            "input_file": {
+                                "filename": "some/path"
+                            },
+                            "start_col": 5,
+                            "start_line": 7
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+	zeroProgram, err := ZeroProgramFromJSON(content)
+	require.NoError(t, err)
+
+	require.Equal(
+		t,
+		&ZeroProgram{
+			DebugInfo: DebugInfo{
+				FileContents: map[string]string{
+					"key1": "value1",
+				},
+				InstructionLocations: map[string]InstructionLocation{
+					"7": {
+						AccessibleScopes: []string{
+							"starkware.cairo.lang.compiler.lib.registers",
+							"starkware.cairo.lang.compiler.lib.registers.get_fp_and_pc",
+						},
+						FlowTrackingData: FlowTrackingData{
+							ApTracking: ApTracking{
+								Group:  0,
+								Offset: 0,
+							},
+							ReferenceIds: map[string]uint64{
+								"starkware.cairo.lang.compiler.lib.registers.get_ap.fp_val": 0,
+								"starkware.cairo.lang.compiler.lib.registers.get_ap.pc_val": 1,
+							},
+						},
+						Hints: []HintLocation{
+							{
+								Location: Location{
+									EndCol:  38,
+									EndLine: 3,
+									InputFile: map[string]string{
+										"filename": "some/path",
+									},
+									StartCol:  5,
+									StartLine: 3,
+								},
+								NPrefixNewlines: 0,
+							},
+						},
+						Inst: Location{
+							EndCol:  73,
+							EndLine: 7,
+							InputFile: map[string]string{
+								"filename": "some/path",
+							},
+							StartCol:  5,
+							StartLine: 7,
+						},
+					},
+				},
+			},
+		},
+		zeroProgram,
+	)
 }
