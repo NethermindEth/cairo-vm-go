4bd501975b6b7b8bf2e7b280abcf01fee84f3a61
jmjac <jakub@jackowiak.dev>
2023-09-21 17:02:48 -0400

    Add cell read and write tests

--- pkg/vm/memory/memory_test.go
+++ pkg/vm/memory/memory_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWrite(t *testing.T) {
+func TestCellWrite(t *testing.T) {
 	cell := Cell{}
 
 	err := cell.Write(MemoryValueFromInt(1)) // Write 1 to a new cell
@@ -22,7 +22,7 @@ func TestWrite(t *testing.T) {
 	assert.Equal(t, cell.Value, MemoryValueFromInt(1)) //check that the value didn't change
 }
 
-func TestRead(t *testing.T) {
+func TestCellRead(t *testing.T) {
 	cell := Cell{Accessed: false, Value: nil}
 	assert.Equal(t, cell.Read(), EmptyMemoryValueAsFelt()) //Read from empty cell
 	assert.True(t, cell.Accessed)
@@ -32,7 +32,7 @@ func TestRead(t *testing.T) {
 	assert.True(t, cell.Accessed)
 }
 
-func TestWriteAndRead(t *testing.T) {
+func TestCellWriteAndRead(t *testing.T) {
 	cell := Cell{}
 
 	err := cell.Write(MemoryValueFromInt(82))
@@ -40,5 +40,48 @@ func TestWriteAndRead(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, cell.Accessed)
 	assert.Equal(t, cell.Read(), MemoryValueFromInt(82))
+}
+
+func TestSegmentRead(t *testing.T) {
+	segment := Segment{Data: []Cell{
+		{Accessed: false, Value: MemoryValueFromInt(3)},
+		{Accessed: false, Value: MemoryValueFromInt(5)},
+		{Accessed: true, Value: MemoryValueFromInt(9)},
+	}}
+
+	assert.Equal(t, segment.Read(0), MemoryValueFromInt(3))
+	assert.Equal(t, segment.Read(1), MemoryValueFromInt(5))
+	assert.Equal(t, segment.Read(2), MemoryValueFromInt(9))
+	assert.True(t, segment.Data[0].Accessed) //Segment read should mark cell as accessed
+	assert.True(t, segment.Data[1].Accessed)
+	assert.True(t, segment.Data[2].Accessed)
+
+	assert.Equal(t, len(segment.Data), 3)
+	//Check if we can read offsets higher than segment len
+	assert.Equal(t, segment.Read(100), EmptyMemoryValueAsFelt())
+	assert.Equal(t, len(segment.Data), 101) //Verify that segment len was increased
+}
+
+func TestSegmentPeek(t *testing.T) {
+	segment := Segment{Data: []Cell{
+		{Accessed: false, Value: MemoryValueFromInt(2)},
+		{Accessed: true, Value: MemoryValueFromInt(4)},
+	}}
+	assert.Equal(t, segment.Peek(0).Value, MemoryValueFromInt(2))
+	assert.Equal(t, segment.Peek(1).Value, MemoryValueFromInt(4))
+	assert.False(t, segment.Data[0].Accessed) //Peek should not mark the cell as accessed
+	assert.True(t, segment.Data[1].Accessed)  //Cell that was already accessed should stay accessed
+
+	assert.Equal(t, len(segment.Data), 2)
+	//Check if we can peek offsets higher than segment len
+	assert.Equal(t, segment.Peek(30).Read(), EmptyMemoryValueAsFelt())
+	assert.Equal(t, len(segment.Data), 31) //Verify that segment len was increased
+}
+
+func TestSegmentWrite(t *testing.T) {
+
+}
+
+func TestSegmentReadAndWrite(t *testing.T) {
 
 }
