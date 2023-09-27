49f0c5956d8d178a3bb8ae5a5b79ff40d6410c5c
Rodrigo <rodrodpino@gmail.com>
2023-09-27 19:31:59 -0400

    Feat: Memory Management Optimization (#73)
    * Remove useless make commands
    
    * Split logic components in zero runner
    
    * Add benchmakrs to go test
    
    * Add generic max function
    
    * Optimize memory management
    
    * reflect optimization on mem manager
    
    * Add new tests for memory
    
    * Bug fix
    
    * Trim test segments

--- pkg/runners/zero/zero_test.go
+++ pkg/runners/zero/zero_test.go
@@ -14,59 +14,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestLoadCairoZeroProgram(t *testing.T) {
-	content := []byte(`
-        {
-            "data": [
-                "0x0000001",
-                "0x0000002",
-                "0x0000003",
-                "0x0000004"
-            ],
-            "main_scope": "__main__",
-            "identifiers": {
-                "__main__.main": {
-                    "decorators": [],
-                    "pc": 0,
-                    "type": "function"
-                },
-                "__main__.fib": {
-                    "decorators": [],
-                    "pc": 4,
-                    "type": "function"
-                }
-            }
-        }
-    `)
-
-	stringToFelt := func(bytecode string) *f.Element {
-		felt, err := new(f.Element).SetString(bytecode)
-		if err != nil {
-			panic(err)
-		}
-		return felt
-	}
-
-	program, err := LoadCairoZeroProgram(content)
-	require.NoError(t, err)
-
-	require.Equal(t, &Program{
-		Bytecode: []*f.Element{
-			stringToFelt("0x01"),
-			stringToFelt("0x02"),
-			stringToFelt("0x03"),
-			stringToFelt("0x04"),
-		},
-		Entrypoints: map[string]uint64{
-			"main": 0,
-			"fib":  4,
-		},
-		Labels: map[string]uint64{},
-	},
-		program,
-	)
-}
-
 func TestSimpleProgram(t *testing.T) {
 	program := createDefaultProgram(`
         [ap] = 2, ap++;
@@ -104,7 +51,7 @@ func TestSimpleProgram(t *testing.T) {
 			4,
 			4,
 		),
-		executionSegment,
+		trimmedSegment(executionSegment),
 	)
 
 	assert.Equal(t, uint64(5), runner.vm.Context.Ap)
@@ -148,7 +95,7 @@ func TestStepLimitExceeded(t *testing.T) {
 			3,
 			5,
 		),
-		executionSegment,
+		trimmedSegment(executionSegment),
 	)
 
 	// when running on non proof mode, the first to elements
@@ -206,7 +153,7 @@ func TestStepLimitExceededProofMode(t *testing.T) {
 				11,
 				13,
 			),
-			executionSegment,
+			trimmedSegment(executionSegment),
 		)
 
 		// when running on non proof mode, the first to elements
@@ -310,6 +257,230 @@ func TestMemoryEncodingDecoding(t *testing.T) {
 	)
 }
 
+func BenchmarkRunnerWithFibonacci(b *testing.B) {
+	compiledJson := []byte(`
+        {
+            "compiler_version": "0.11.0.2",
+            "data": [
+                "0x40780017fff7fff",
+                "0x0",
+                "0x1104800180018000",
+                "0x4",
+                "0x10780017fff7fff",
+                "0x0",
+                "0x480680017fff8000",
+                "0x1",
+                "0x480680017fff8000",
+                "0x1",
+                "0x480680017fff8000",
+                "0xf4240",
+                "0x1104800180018000",
+                "0x3",
+                "0x208b7fff7fff7ffe",
+                "0x20780017fff7ffd",
+                "0x4",
+                "0x480a7ffc7fff8000",
+                "0x208b7fff7fff7ffe",
+                "0x482a7ffc7ffb8000",
+                "0x480a7ffc7fff8000",
+                "0x48127ffe7fff8000",
+                "0x482680017ffd8000",
+                "0x800000000000011000000000000000000000000000000000000000000000000",
+                "0x1104800180018000",
+                "0x800000000000010fffffffffffffffffffffffffffffffffffffffffffffff8",
+                "0x208b7fff7fff7ffe"
+            ],
+            "identifiers": {
+                "__main__.__end__": {
+                    "pc": 4,
+                    "type": "label"
+                },
+                "__main__.__start__": {
+                    "pc": 0,
+                    "type": "label"
+                },
+                "__main__.fib": {
+                    "decorators": [],
+                    "pc": 15,
+                    "type": "function"
+                },
+                "__main__.fib.Args": {
+                    "full_name": "__main__.fib.Args",
+                    "members": {
+                        "first_element": {
+                            "cairo_type": "felt",
+                            "offset": 0
+                        },
+                        "n": {
+                            "cairo_type": "felt",
+                            "offset": 2
+                        },
+                        "second_element": {
+                            "cairo_type": "felt",
+                            "offset": 1
+                        }
+                    },
+                    "size": 3,
+                    "type": "struct"
+                },
+                "__main__.fib.ImplicitArgs": {
+                    "full_name": "__main__.fib.ImplicitArgs",
+                    "members": {},
+                    "size": 0,
+                    "type": "struct"
+                },
+                "__main__.fib.Return": {
+                    "cairo_type": "(res: felt)",
+                    "type": "type_definition"
+                },
+                "__main__.fib.SIZEOF_LOCALS": {
+                    "type": "const",
+                    "value": 0
+                },
+                "__main__.fib.first_element": {
+                    "cairo_type": "felt",
+                    "full_name": "__main__.fib.first_element",
+                    "references": [
+                        {
+                            "ap_tracking_data": {
+                                "group": 4,
+                                "offset": 0
+                            },
+                            "pc": 15,
+                            "value": "[cast(fp + (-5), felt*)]"
+                        }
+                    ],
+                    "type": "reference"
+                },
+                "__main__.fib.n": {
+                    "cairo_type": "felt",
+                    "full_name": "__main__.fib.n",
+                    "references": [
+                        {
+                            "ap_tracking_data": {
+                                "group": 4,
+                                "offset": 0
+                            },
+                            "pc": 15,
+                            "value": "[cast(fp + (-3), felt*)]"
+                        }
+                    ],
+                    "type": "reference"
+                },
+                "__main__.fib.second_element": {
+                    "cairo_type": "felt",
+                    "full_name": "__main__.fib.second_element",
+                    "references": [
+                        {
+                            "ap_tracking_data": {
+                                "group": 4,
+                                "offset": 0
+                            },
+                            "pc": 15,
+                            "value": "[cast(fp + (-4), felt*)]"
+                        }
+                    ],
+                    "type": "reference"
+                },
+                "__main__.fib.y": {
+                    "cairo_type": "felt",
+                    "full_name": "__main__.fib.y",
+                    "references": [
+                        {
+                            "ap_tracking_data": {
+                                "group": 4,
+                                "offset": 1
+                            },
+                            "pc": 20,
+                            "value": "[cast(ap + (-1), felt*)]"
+                        }
+                    ],
+                    "type": "reference"
+                },
+                "__main__.main": {
+                    "decorators": [],
+                    "pc": 6,
+                    "type": "function"
+                },
+                "__main__.main.Args": {
+                    "full_name": "__main__.main.Args",
+                    "members": {},
+                    "size": 0,
+                    "type": "struct"
+                },
+                "__main__.main.ImplicitArgs": {
+                    "full_name": "__main__.main.ImplicitArgs",
+                    "members": {},
+                    "size": 0,
+                    "type": "struct"
+                },
+                "__main__.main.Return": {
+                    "cairo_type": "()",
+                    "type": "type_definition"
+                },
+                "__main__.main.SIZEOF_LOCALS": {
+                    "type": "const",
+                    "value": 0
+                }
+            },
+            "main_scope": "__main__",
+            "prime": "0x800000000000011000000000000000000000000000000000000000000000001",
+            "reference_manager": {
+                "references": [
+                    {
+                        "ap_tracking_data": {
+                            "group": 4,
+                            "offset": 0
+                        },
+                        "pc": 15,
+                        "value": "[cast(fp + (-5), felt*)]"
+                    },
+                    {
+                        "ap_tracking_data": {
+                            "group": 4,
+                            "offset": 0
+                        },
+                        "pc": 15,
+                        "value": "[cast(fp + (-4), felt*)]"
+                    },
+                    {
+                        "ap_tracking_data": {
+                            "group": 4,
+                            "offset": 0
+                        },
+                        "pc": 15,
+                        "value": "[cast(fp + (-3), felt*)]"
+                    },
+                    {
+                        "ap_tracking_data": {
+                            "group": 4,
+                            "offset": 1
+                        },
+                        "pc": 20,
+                        "value": "[cast(ap + (-1), felt*)]"
+                    }
+                ]
+            }
+        }
+    `)
+	for i := 0; i < b.N; i++ {
+		program, err := LoadCairoZeroProgram(compiledJson)
+		if err != nil {
+			panic(err)
+		}
+
+		runner, err := NewRunner(program, true, math.MaxUint64)
+		if err != nil {
+			panic(err)
+		}
+
+		err = runner.Run()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func createSegment(values ...any) *memory.Segment {
 	data := make([]*memory.Cell, len(values))
 	for i := range values {
@@ -322,8 +493,16 @@ func createSegment(values ...any) *memory.Segment {
 		}
 	}
 	return &memory.Segment{
-		Data: data,
+		Data:      data,
+		LastIndex: len(data) - 1,
 	}
+}
+
+// modifies a segment in place to reduce its real length to
+// its effective lenth. It returns the same segment
+func trimmedSegment(segment *memory.Segment) *memory.Segment {
+	segment.Data = segment.Data[0:segment.Len()]
+	return segment
 }
 
 func createDefaultProgram(code string) *Program {
