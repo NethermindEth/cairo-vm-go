4db2acdf664e8187270774f057d43fb063a75f0d
Ömer Faruk Irmak <omerfirmak@gmail.com>
2023-10-03 16:21:34 +0300

    Fix segment bound checks (#83)

--- pkg/vm/memory/memory.go
+++ pkg/vm/memory/memory.go
@@ -184,7 +184,7 @@ func (memory *Memory) AllocateEmptySegment() int {
 // Writes to a memory address a new memory value. Errors if writing to an unallocated
 // space or if rewriting a specific cell
 func (memory *Memory) Write(segmentIndex uint64, offset uint64, value *MemoryValue) error {
-	if segmentIndex > uint64(len(memory.Segments)) {
+	if segmentIndex >= uint64(len(memory.Segments)) {
 		return fmt.Errorf("unallocated segment at index %d", segmentIndex)
 	}
 	return memory.Segments[segmentIndex].Write(offset, value)
@@ -198,7 +198,7 @@ func (memory *Memory) WriteToAddress(address *MemoryAddress, value *MemoryValue)
 // an unallocated space. If reading a cell which hasn't been accesed before, it is
 // initalized with its default zero value
 func (memory *Memory) Read(segmentIndex uint64, offset uint64) (MemoryValue, error) {
-	if segmentIndex > uint64(len(memory.Segments)) {
+	if segmentIndex >= uint64(len(memory.Segments)) {
 		return MemoryValue{}, fmt.Errorf("unallocated segment at index %d", segmentIndex)
 	}
 	return memory.Segments[segmentIndex].Read(offset), nil
@@ -213,7 +213,7 @@ func (memory *Memory) ReadFromAddress(address *MemoryAddress) (MemoryValue, erro
 
 // Given a segment index and offset returns a pointer to the Memory Cell
 func (memory *Memory) Peek(segmentIndex uint64, offset uint64) (MemoryValue, error) {
-	if segmentIndex > uint64(len(memory.Segments)) {
+	if segmentIndex >= uint64(len(memory.Segments)) {
 		return MemoryValue{}, fmt.Errorf("unallocated segment at index %d", segmentIndex)
 	}
 	return memory.Segments[segmentIndex].Peek(offset), nil
