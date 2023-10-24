570208ac49ca508e86d8426fa483cf34215f49f8
jmjac <jakub@jackowiak.dev>
2023-10-23 21:27:28 -0400

    Add lessThan benchmark and fix AllocSegment benchmark error

--- pkg/hintrunner/hint_bechmark_test.go
+++ pkg/hintrunner/hint_bechmark_test.go
@@ -1,17 +1,54 @@
 package hintrunner
 
 import (
+	"math/big"
 	"testing"
+
+	VM "github.com/NethermindEth/cairo-vm-go/pkg/vm"
+	"github.com/NethermindEth/cairo-vm-go/pkg/vm/memory"
 )
 
 func BenchmarkAllocSegment(b *testing.B) {
 	vm := defaultVirtualMachine()
 	vm.Context.Ap = 0
 	vm.Context.Fp = 0
-	var ap ApCellRef = 5
+	var ap ApCellRef = 1
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		alloc := AllocSegment{ap}
-		_ = alloc.Execute(vm)
-		ap += 1
+		err := alloc.Execute(vm)
+		if err != nil {
+			panic(err)
+		}
+
+		vm.Context.Ap += 1
 	}
+}
+
+func BenchmarkLessThan(b *testing.B) {
+	vm := defaultVirtualMachine()
+	vm.Context.Ap = 0
+	vm.Context.Fp = 0
+
+	var dst ApCellRef = 1
+	var rhsRef ApCellRef = 2
+	cell := uint64(0)
+	for i := 0; i < b.N; i++ {
+		writeTo(vm, VM.ExecutionSegment, vm.Context.Ap+uint64(rhsRef), memory.MemoryValueFromInt(int64(23*cell)%13))
+		rhs := Deref{rhsRef}
+		lhs := Immediate(*big.NewInt(int64((13 * cell) % 11)))
+		hint := TestLessThan{
+			dst: dst,
+			lhs: lhs,
+			rhs: rhs,
+		}
+		err := hint.Execute(vm)
+		if err != nil {
+			panic(err)
+		}
+
+		vm.Context.Ap += 3
+		cell += 1
+	}
+
 }
