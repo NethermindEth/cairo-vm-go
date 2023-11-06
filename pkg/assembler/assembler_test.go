1faacdeb5cc95e03292252f3f818b9a28f6c4664
Mahdi Khosravi <mmk1776@gmail.com>
2023-11-06 16:36:36 +0300

    minor fix to assembler_test.go

--- pkg/assembler/assembler_test.go
+++ pkg/assembler/assembler_test.go
@@ -544,7 +544,7 @@ func TestCallAbsNegative(t *testing.T) {
 	expectedImm := f.Element{}
 	expectedImm.SetInt64(-123)
 	// verify imm
-	assert.Equal(t, expectedImm.Bits(), imm.Bits())
+	assert.Equal(t, expectedImm, imm)
 
 	// verify offsets
 	dstOffset := uint16(encode)
