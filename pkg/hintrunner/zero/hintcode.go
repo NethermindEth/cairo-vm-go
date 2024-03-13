23c5939beb3a92789666898e568e782676ac1b01
MaksymMalicki <81577596+MaksymMalicki@users.noreply.github.com>
2024-03-13 19:09:35 +0100

    Implement Uint256SignedNN hint (#236)
    * Implement Uint256SignedNN hint
    
    * Implement suggestions from PR review
    
    * Add comment from PR conversation
    
    * Fixed lint issues

--- pkg/hintrunner/zero/hintcode.go
+++ pkg/hintrunner/zero/hintcode.go
@@ -38,6 +38,7 @@ const (
 	uint256AddCode    string = "sum_low = ids.a.low + ids.b.low\nids.carry_low = 1 if sum_low >= ids.SHIFT else 0\nsum_high = ids.a.high + ids.b.high + ids.carry_low\nids.carry_high = 1 if sum_high >= ids.SHIFT else 0"
 	uint256AddLowCode string = "sum_low = ids.a.low + ids.b.low\nids.carry_low = 1 if sum_low >= ids.SHIFT else 0"
 	split64Code       string = "ids.low = ids.a & ((1<<64) - 1)\nids.high = ids.a >> 64"
+	uint256SignedNN   string = "memory[ap] = 1 if 0 <= (ids.a.high % PRIME) < 2 ** 127 else 0"
 
 	// ------ Usort hints related code ------
 
