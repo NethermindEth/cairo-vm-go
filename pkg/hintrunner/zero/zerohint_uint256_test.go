c423a01384f2acecf161a693b1adfb865fcdff5a
MaksymMalicki <maksym.malicki.2115@gmail.com>
2024-03-19 14:14:40 +0100

    Added new test cases, refactored code

--- pkg/hintrunner/zero/zerohint_uint256_test.go
+++ pkg/hintrunner/zero/zerohint_uint256_test.go
@@ -155,33 +155,33 @@ func TestZeroHintUint256(t *testing.T) {
 			},
 		},
 		"Uint256MulDivMod": {
-			// {
-			// 	operanders: []*hintOperander{
-			// 		{Name: "a.low", Kind: apRelative, Value: feltUint64(6)},
-			// 		{Name: "a.high", Kind: apRelative, Value: feltUint64(0)},
-			// 		{Name: "b.low", Kind: apRelative, Value: feltUint64(6)},
-			// 		{Name: "b.high", Kind: apRelative, Value: feltUint64(0)},
-			// 		{Name: "div.low", Kind: apRelative, Value: feltUint64(2)},
-			// 		{Name: "div.high", Kind: apRelative, Value: feltUint64(0)},
-			// 		{Name: "quotient_low.low", Kind: uninitialized},
-			// 		{Name: "quotient_low.high", Kind: uninitialized},
-			// 		{Name: "quotient_high.low", Kind: uninitialized},
-			// 		{Name: "quotient_high.high", Kind: uninitialized},
-			// 		{Name: "remainder.low", Kind: uninitialized},
-			// 		{Name: "remainder.high", Kind: uninitialized},
-			// 	},
-			// 	makeHinter: func(ctx *hintTestContext) hinter.Hinter {
-			// 		return newUint256MulDivModHint(ctx.operanders["a.low"], ctx.operanders["b.low"], ctx.operanders["div.low"], ctx.operanders["quotient_low.low"], ctx.operanders["quotient_high.low"], ctx.operanders["remainder.low"])
-			// 	},
-			// 	check: allVarValueEquals(map[string]*fp.Element{
-			// 		"quotient_low.low":   feltUint64(18),
-			// 		"quotient_low.high":  feltUint64(0),
-			// 		"quotient_high.low":  feltUint64(0),
-			// 		"quotient_high.high": feltUint64(0),
-			// 		"remainder.low":      feltUint64(0),
-			// 		"remainder.high":     feltUint64(0),
-			// 	}),
-			// },
+			{
+				operanders: []*hintOperander{
+					{Name: "a.low", Kind: apRelative, Value: feltUint64(6)},
+					{Name: "a.high", Kind: apRelative, Value: feltUint64(0)},
+					{Name: "b.low", Kind: apRelative, Value: feltUint64(6)},
+					{Name: "b.high", Kind: apRelative, Value: feltUint64(0)},
+					{Name: "div.low", Kind: apRelative, Value: feltUint64(2)},
+					{Name: "div.high", Kind: apRelative, Value: feltUint64(0)},
+					{Name: "quotient_low.low", Kind: uninitialized},
+					{Name: "quotient_low.high", Kind: uninitialized},
+					{Name: "quotient_high.low", Kind: uninitialized},
+					{Name: "quotient_high.high", Kind: uninitialized},
+					{Name: "remainder.low", Kind: uninitialized},
+					{Name: "remainder.high", Kind: uninitialized},
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newUint256MulDivModHint(ctx.operanders["a.low"], ctx.operanders["b.low"], ctx.operanders["div.low"], ctx.operanders["quotient_low.low"], ctx.operanders["quotient_high.low"], ctx.operanders["remainder.low"])
+				},
+				check: allVarValueEquals(map[string]*fp.Element{
+					"quotient_low.low":   feltUint64(18),
+					"quotient_low.high":  feltUint64(0),
+					"quotient_high.low":  feltUint64(0),
+					"quotient_high.high": feltUint64(0),
+					"remainder.low":      feltUint64(0),
+					"remainder.high":     feltUint64(0),
+				}),
+			},
 			{
 				operanders: []*hintOperander{
 					{Name: "a.low", Kind: apRelative, Value: &utils.FeltZero},
@@ -202,12 +202,32 @@ func TestZeroHintUint256(t *testing.T) {
 				},
 				check: allVarValueEquals(map[string]*fp.Element{
 					"quotient_low.low":   &utils.FeltZero,
-					"quotient_low.high":  &utils.FeltZero,
+					"quotient_low.high":  feltUint64(3),
 					"quotient_high.low":  &utils.FeltZero,
 					"quotient_high.high": &utils.FeltZero,
 					"remainder.low":      &utils.FeltZero,
 					"remainder.high":     &utils.FeltZero,
 				}),
+			},
+			{
+				operanders: []*hintOperander{
+					{Name: "a.low", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "a.high", Kind: apRelative, Value: feltString("2")},
+					{Name: "b.low", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "b.high", Kind: apRelative, Value: feltString("3")},
+					{Name: "div.low", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "div.high", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "quotient_low.low", Kind: uninitialized},
+					{Name: "quotient_low.high", Kind: uninitialized},
+					{Name: "quotient_high.low", Kind: uninitialized},
+					{Name: "quotient_high.high", Kind: uninitialized},
+					{Name: "remainder.low", Kind: uninitialized},
+					{Name: "remainder.high", Kind: uninitialized},
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newUint256MulDivModHint(ctx.operanders["a.low"], ctx.operanders["b.low"], ctx.operanders["div.low"], ctx.operanders["quotient_low.low"], ctx.operanders["quotient_high.low"], ctx.operanders["remainder.low"])
+				},
+				errCheck: errorTextContains("panic: division by zero"),
 			},
 		},
 	})
