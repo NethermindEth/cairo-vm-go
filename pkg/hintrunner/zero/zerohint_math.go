fc3bfb66d1aafeeba0b601b7b6b14176989c8a2f
tomi-3-0 <calebomoniyitomiwa@gmail.com>
2024-03-19 15:09:46 +0100

    add: complete hintcode in python for sqrt hint

--- pkg/hintrunner/zero/zerohint_math.go
+++ pkg/hintrunner/zero/zerohint_math.go
@@ -588,6 +588,7 @@ func newSqrtHint(root, value hinter.ResOperander) hinter.Hinter {
 	return &GenericZeroHinter{
 		Name: "Sqrt",
 		Op: func(vm *VM.VirtualMachine, _ *hinter.HintRunnerContext) error {
+			//> from starkware.python.math_utils import isqrt
 			// value = ids.value % PRIME
 			// assert value < 2 ** 250, f"value={value} is outside of the range [0, 2**250)."
 			// assert 2 ** 250 < PRIME
