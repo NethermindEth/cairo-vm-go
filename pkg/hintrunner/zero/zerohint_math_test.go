2122d6134e16de90d35616a67f51794b661bd759
MaksymMalicki <81577596+MaksymMalicki@users.noreply.github.com>
2024-03-19 08:28:51 +0100

    Cairo0 hint split felt (#304)
    * Implement cairo0 split hint
    
    * Add test cases
    
    * Debug the split-felt hint
    
    * Debug test cases, debug hint basing on tests
    
    * Implement changes from the PR conversation

--- pkg/hintrunner/zero/zerohint_math_test.go
+++ pkg/hintrunner/zero/zerohint_math_test.go
@@ -555,5 +555,36 @@ func TestZeroHintMath(t *testing.T) {
 				errCheck: errorTextContains("outside of the range [0, 2**250)"),
 			},
 		},
+
+		"SplitFelt": {
+			{
+				operanders: []*hintOperander{
+					{Name: "low", Kind: reference, Value: addrBuiltin(starknet.RangeCheck, 0)},
+					{Name: "high", Kind: reference, Value: addrBuiltin(starknet.RangeCheck, 1)},
+					{Name: "value", Kind: apRelative, Value: feltString("100000000000000000000000000000000000000")},
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newSplitFeltHint(ctx.operanders["low"], ctx.operanders["high"], ctx.operanders["value"])
+				},
+				check: allVarValueEquals(map[string]*fp.Element{
+					"low":  feltString("100000000000000000000000000000000000000"),
+					"high": feltInt64(0),
+				}),
+			},
+			{
+				operanders: []*hintOperander{
+					{Name: "low", Kind: reference, Value: addrBuiltin(starknet.RangeCheck, 0)},
+					{Name: "high", Kind: reference, Value: addrBuiltin(starknet.RangeCheck, 1)},
+					{Name: "value", Kind: apRelative, Value: &utils.FeltMax128},
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newSplitFeltHint(ctx.operanders["low"], ctx.operanders["high"], ctx.operanders["value"])
+				},
+				check: allVarValueEquals(map[string]*fp.Element{
+					"low":  feltInt64(0),
+					"high": feltInt64(1),
+				}),
+			},
+		},
 	})
 }
