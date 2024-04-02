6a4dcfeb9f5f1303eaaa38f4a7db904dad1c9e60
MaksymMalicki <maksym.malicki.2115@gmail.com>
2024-04-02 20:43:58 +0200

    Scooped out the deref from double deref in test cases

--- pkg/hintrunner/zero/zerohint_signatures_test.go
+++ pkg/hintrunner/zero/zerohint_signatures_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/NethermindEth/cairo-vm-go/pkg/hintrunner/hinter"
 	"github.com/NethermindEth/cairo-vm-go/pkg/parsers/starknet"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSignatures(t *testing.T) {
@@ -17,20 +18,12 @@ func TestSignatures(t *testing.T) {
 					{Name: "signature_s", Kind: apRelative, Value: feltString("598673427589502599949712887611119751108407514580626464031881322743364689811")},
 				},
 				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
-					return newVerifyECDSASignatureHinter(ctx.operanders["ecdsaPtr"], ctx.operanders["signature_r"], ctx.operanders["signature_s"])
+					ecdsaPtr := ctx.operanders["ecdsaPtr"].(*hinter.DoubleDeref).Deref
+					return newVerifyECDSASignatureHinter(ecdsaPtr, ctx.operanders["signature_r"], ctx.operanders["signature_s"])
 				},
-				errCheck: errorTextContains("outside of the range [0, 2**250)"),
-			},
-			{
-				operanders: []*hintOperander{
-					{Name: "ecdsaPtr", Kind: reference, Value: addrBuiltin(starknet.ECDSA, 0)},
-					{Name: "signature_r", Kind: apRelative, Value: feltString("3086480810278599376317923499561306189851900463386393948998357832163236918254")},
-					{Name: "signature_s", Kind: apRelative, Value: feltString("598673421589502599949712887611119751108407514580626464031881322743364689811")},
+				errCheck: func(t *testing.T, ctx *hintTestContext, err error) {
+					require.NoError(t, err)
 				},
-				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
-					return newVerifyECDSASignatureHinter(ctx.operanders["ecdsaPtr"], ctx.operanders["signature_r"], ctx.operanders["signature_s"])
-				},
-				errCheck: errorTextContains("outside of the range [0, 2**250)"),
 			},
 		},
 	})
