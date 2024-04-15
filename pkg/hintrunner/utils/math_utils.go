d50bca65c772c36627177d63e303dd65c9e199a0
MaksymMalicki <81577596+MaksymMalicki@users.noreply.github.com>
2024-04-15 15:22:27 +0200

    Cairo0 get point from x hint (#340)
    * Implement VerifyECDSASignature hint
    
    * Fixed retrieving memory with signature via pointer
    
    * Scooped out the deref from double deref in test cases
    
    * Implement GetPointFromXHint
    
    * Modify zerohint.go
    
    * Add testcases to the hint
    
    * Add final testcases
    
    * Changes from merging main
    
    * Remove faulty integration tests
    
    * Changes named in the PR discussion
    
    * Fixes after merge
    
    * Fixes from conversation

--- pkg/hintrunner/utils/math_utils.go
+++ pkg/hintrunner/utils/math_utils.go
@@ -11,11 +11,15 @@ func AsInt(valueFelt *fp.Element) *big.Int {
 
 	var valueBig big.Int
 	valueFelt.BigInt(&valueBig)
+	return AsIntBig(&valueBig)
+}
+
+func AsIntBig(value *big.Int) *big.Int {
 	boundBig := new(big.Int).Div(fp.Modulus(), big.NewInt(2))
 
 	// val if val < prime // 2 else val - prime
-	if valueBig.Cmp(boundBig) == -1 {
-		return &valueBig
+	if value.Cmp(boundBig) == -1 {
+		return value
 	}
-	return new(big.Int).Sub(&valueBig, fp.Modulus())
+	return new(big.Int).Sub(value, fp.Modulus())
 }
