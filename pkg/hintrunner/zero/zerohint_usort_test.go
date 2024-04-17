943ea5a379ffa3713010ab0dec2b30a04c6adaf8
Daniel Kyutae Jung <pxyxyrus@gmail.com>
2024-04-16 22:02:51 -0700

    UsortVerify test draft

--- pkg/hintrunner/zero/zerohint_usort_test.go
+++ pkg/hintrunner/zero/zerohint_usort_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/NethermindEth/cairo-vm-go/pkg/hintrunner/hinter"
+	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
+	"github.com/stretchr/testify/require"
 )
 
 func TestZeroHintUsort(t *testing.T) {
@@ -19,6 +21,42 @@ func TestZeroHintUsort(t *testing.T) {
 					return newUsortEnterScopeHint()
 				},
 				check: varValueInScopeEquals("__usort_max_size", feltUint64(1)),
+			},
+		},
+		"UsortVerify": {
+			{
+				ctxInit: func(ctx *hinter.HintRunnerContext) {
+					hinter.InitializeScopeManager(ctx, map[string]any{
+						"positions_dict": map[uint64][]*fp.Element{
+							0: {feltUint64(1), feltUint64(2), feltUint64(3)},
+						},
+					})
+				},
+				operanders: []*hintOperander{
+					{Name: "value", Kind: fpRelative, Value: feltUint64(0)},
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newUsortVerifyHinter(ctx.operanders["value"])
+				},
+				check: func(t *testing.T, ctx *hintTestContext) {
+					positions, err := ctx.runnerContext.ScopeManager.GetVariableValue("positions")
+					require.NoError(t, err)
+
+					require.Equal(t, [...]*fp.Element{feltUint64(3), feltUint64(2), feltUint64(1)}, positions)
+
+					lastPos, err := ctx.runnerContext.ScopeManager.GetVariableValue("last_pos")
+					require.NoError(t, err)
+
+					require.Equal(t, 0, lastPos)
+				},
+				// check: allVarValueInScopeEquals(map[string]any{
+				// 	"last_pos":  0,
+				// 	"positions": []*fp.Element{feltUint64(3), feltUint64(2), feltUint64(1)},
+				// }),
+				// check: allVarValueInScopeEquals(map[string]any{
+				// 	"last_pos":  0,
+				// 	"positions": [...]*fp.Element{feltUint64(3), feltUint64(2), feltUint64(1)},
+				// }),
 			},
 		},
 	})
