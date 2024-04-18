7d5edb3a47577e02aaaa514deda24423a631f19b
TAdev0 <angles.tristan@protonmail.com>
2024-04-18 16:01:17 +0200

    annotate failing tests

--- pkg/hintrunner/zero/zerohint_ec_test.go
+++ pkg/hintrunner/zero/zerohint_ec_test.go
@@ -13,41 +13,41 @@ import (
 func TestZeroHintEc(t *testing.T) {
 	runHinterTests(t, map[string][]hintTestCase{
 		"EcNegate": {
-			// {
-			// 	operanders: []*hintOperander{
-			// 		// random values
-			// 		{Name: "x.d0", Kind: apRelative, Value: feltString("0xe28d959f2815b16f81798")},
-			// 		{Name: "x.d1", Kind: apRelative, Value: feltString("0xa573a1c2c1c0a6ff36cb7")},
-			// 		{Name: "x.d2", Kind: apRelative, Value: feltString("0x79be667ef9dcbbac55a06")},
-			// 		{Name: "y.d0", Kind: apRelative, Value: feltString("0x554199c47d08ffb10d4b8")},
-			// 		{Name: "y.d1", Kind: apRelative, Value: feltString("0x2ff0384422a3f45ed1229a")},
-			// 		{Name: "y.d2", Kind: apRelative, Value: feltString("0x483ada7726a3c4655da4f")},
-			// 	},
-			// 	ctxInit: func(ctx *hinter.HintRunnerContext) {
-			// 		ctx.ScopeManager.EnterScope(map[string]any{})
-			// 	},
-			// 	makeHinter: func(ctx *hintTestContext) hinter.Hinter {
-			// 		return newEcNegateHint(ctx.operanders["x.d0"])
-			// 	},
-			// 	check: varValueInScopeEquals("value", bigIntString("83121579216557378445487899878180864668798711284981320763518679672151497189239", 10)),
-			// },
-			// {
-			// 	operanders: []*hintOperander{
-			// 		{Name: "x.d0", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "x.d1", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "x.d2", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "y.d0", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "y.d1", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "y.d2", Kind: apRelative, Value: &utils.FeltZero},
-			// 	},
-			// 	ctxInit: func(ctx *hinter.HintRunnerContext) {
-			// 		ctx.ScopeManager.EnterScope(map[string]any{})
-			// 	},
-			// 	makeHinter: func(ctx *hintTestContext) hinter.Hinter {
-			// 		return newEcNegateHint(ctx.operanders["x.d0"])
-			// 	},
-			// 	check: varValueInScopeEquals("value", bigIntString("0", 10)),
-			// },
+			{
+				operanders: []*hintOperander{
+					// random values
+					{Name: "x.d0", Kind: apRelative, Value: feltString("0xe28d959f2815b16f81798")},
+					{Name: "x.d1", Kind: apRelative, Value: feltString("0xa573a1c2c1c0a6ff36cb7")},
+					{Name: "x.d2", Kind: apRelative, Value: feltString("0x79be667ef9dcbbac55a06")},
+					{Name: "y.d0", Kind: apRelative, Value: feltString("0x554199c47d08ffb10d4b8")},
+					{Name: "y.d1", Kind: apRelative, Value: feltString("0x2ff0384422a3f45ed1229a")},
+					{Name: "y.d2", Kind: apRelative, Value: feltString("0x483ada7726a3c4655da4f")},
+				},
+				ctxInit: func(ctx *hinter.HintRunnerContext) {
+					ctx.ScopeManager.EnterScope(map[string]any{})
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newEcNegateHint(ctx.operanders["x.d0"])
+				},
+				check: varValueInScopeEquals("value", bigIntString("83121579216557378445487899878180864668798711284981320763518679672151497189239", 10)),
+			},
+			{
+				operanders: []*hintOperander{
+					{Name: "x.d0", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "x.d1", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "x.d2", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "y.d0", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "y.d1", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "y.d2", Kind: apRelative, Value: &utils.FeltZero},
+				},
+				ctxInit: func(ctx *hinter.HintRunnerContext) {
+					ctx.ScopeManager.EnterScope(map[string]any{})
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newEcNegateHint(ctx.operanders["x.d0"])
+				},
+				check: varValueInScopeEquals("value", bigIntString("0", 10)),
+			},
 			// {
 			// 	operanders: []*hintOperander{
 			// 		// GetSecPUint256() % fp.Modulus()
@@ -174,23 +174,23 @@ func TestZeroHintEc(t *testing.T) {
 			// 	},
 			// 	check: varValueInScopeEquals("value", bigIntString("29125758092381702368368330622702595285804526265760842241803249", 10)),
 			// },
-			// {
-			// 	operanders: []*hintOperander{
-			// 		{Name: "x.d0", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "x.d1", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "x.d2", Kind: apRelative, Value: &utils.FeltZero},
-			// 		{Name: "y.d0", Kind: apRelative, Value: feltString("10")},
-			// 		{Name: "y.d1", Kind: apRelative, Value: feltString("100")},
-			// 		{Name: "y.d2", Kind: apRelative, Value: feltString("10001")},
-			// 	},
-			// 	ctxInit: func(ctx *hinter.HintRunnerContext) {
-			// 		ctx.ScopeManager.EnterScope(map[string]any{})
-			// 	},
-			// 	makeHinter: func(ctx *hintTestContext) hinter.Hinter {
-			// 		return newEcNegateHint(ctx.operanders["x.d0"])
-			// 	},
-			// 	check: varValueInScopeEquals("value", bigIntString("115792089237316195423511115915312127562362008772591693155831694873530722155557", 10)),
-			// },
+			{
+				operanders: []*hintOperander{
+					{Name: "x.d0", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "x.d1", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "x.d2", Kind: apRelative, Value: &utils.FeltZero},
+					{Name: "y.d0", Kind: apRelative, Value: feltString("10")},
+					{Name: "y.d1", Kind: apRelative, Value: feltString("100")},
+					{Name: "y.d2", Kind: apRelative, Value: feltString("10001")},
+				},
+				ctxInit: func(ctx *hinter.HintRunnerContext) {
+					ctx.ScopeManager.EnterScope(map[string]any{})
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newEcNegateHint(ctx.operanders["x.d0"])
+				},
+				check: varValueInScopeEquals("value", bigIntString("115792089237316195423511115915312127562362008772591693155831694873530722155557", 10)),
+			},
 		},
 		"NondetBigint3V1": {
 			{
