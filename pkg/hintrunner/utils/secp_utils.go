fd3723a5140a95f0fc18eeca2d0cb5d9a7f74ced
Harikrishnan Shaji <hihari777@gmail.com>
2024-04-19 18:47:00 +0530

    Implement Ecdoubleslopev1 hint (#341)
    * Implement EcNegate hint
    
    * Implement NondetBigint3V1 hint
    
    * Fix context usage + add some functions to return constants
    
    * Add scope check test utils + usage
    
    * More comments and tests
    
    * More tests
    
    * Clean up some code
    
    * Implement FastEcAddAssignNewY hint
    
    * Implement FastEcAddAssignNewX hint
    
    * Make hint code less verbose
    
    * Add more tests
    
    * Fix comment formatting
    
    * Clean up EcNegate using new util functions
    
    * Implement EcDoubleSlopeV1 hint
    
    * Use FeltZero/FeltOne in tests
    
    * Use FeltZero in tests
    
    * Add secp util functions to clean up hint
    
    * Make function private
    
    * Moving comments around
    
    * Fix import
    
    * Change zero compare
    
    * Make return simpler
    
    * Cleaner return
    
    * Add more tests
    
    * Comment test values
    
    * Comment test values
    
    * Comment test values
    
    * Comment test values
    
    * Update comment
    
    * Simpler context init in tests
    
    * Add GetVariableValueAsBigInt method to scope manager
    
    * Simpler tests
    
    * Remove duplicate test check
    
    * Clean up scope usage
    
    * Use existing constant
    
    * Simpler loop
    
    * Add AssignVariables method to scope manager + use it
    
    * Use AssignVariables in FastEcAddAssignNewY
    
    * Use util methods to make code simpler
    
    * Use feltUint64 when applicable
    
    * Change argument to accept slice
    
    * Add tests for div_mod and igcdex
    
    * Fetch SECP_P from scope in FastEcAddAssignNewY
    
    * Use InitializeScopeManager
    
    * Return big.Int instead of *big.Int
    
    * More return big.Int instead of *big.Int
    
    * Rename to divmod
    
    * Change signature of EcDoubleSlope
    
    * More return big.Int instead of *big.Int
    
    * Remove redundant tests
    
    * Fix some nits
    
    * Return a new big.Int for sign function
    
    ---------
    
    Co-authored-by: Carmen Cabrera <kr1000a@gmail.com>

--- pkg/hintrunner/utils/secp_utils.go
+++ pkg/hintrunner/utils/secp_utils.go
@@ -8,33 +8,35 @@ import (
 	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
 )
 
-func getBaseBig() (*big.Int, bool) {
+func getBaseBig() (big.Int, bool) {
 	// 2**86
-	return new(big.Int).SetString("77371252455336267181195264", 10)
+	base, ok := new(big.Int).SetString("77371252455336267181195264", 10)
+	return *base, ok
 }
 
-func GetSecPBig() (*big.Int, bool) {
+func GetSecPBig() (big.Int, bool) {
 	// 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
-	return new(big.Int).SetString("115792089237316195423570985008687907853269984665640564039457584007908834671663", 10)
+	secP, ok := new(big.Int).SetString("115792089237316195423570985008687907853269984665640564039457584007908834671663", 10)
+	return *secP, ok
 }
 
-func SecPPacked(limbs [3]*fp.Element) (*big.Int, error) {
+func SecPPacked(limbs [3]*fp.Element) (big.Int, error) {
 	// https://github.com/starkware-libs/cairo-lang/blob/efa9648f57568aad8f8a13fbf027d2de7c63c2c0/src/starkware/cairo/common/cairo_secp/secp_utils.py#L28
 
 	baseBig, ok := getBaseBig()
 	if !ok {
-		return nil, fmt.Errorf("getBaseBig failed")
+		return *big.NewInt(0), fmt.Errorf("getBaseBig failed")
 	}
 
 	packedBig := new(big.Int)
 	for idx, limb := range limbs {
 		limbBig := AsInt(limb)
-		valueToAddBig := new(big.Int).Exp(baseBig, big.NewInt(int64(idx)), nil)
+		valueToAddBig := new(big.Int).Exp(&baseBig, big.NewInt(int64(idx)), nil)
 		valueToAddBig.Mul(valueToAddBig, &limbBig)
 		packedBig.Add(packedBig, valueToAddBig)
 	}
 
-	return packedBig, nil
+	return *packedBig, nil
 }
 
 func GetBetaBig() big.Int {
@@ -48,10 +50,10 @@ func GetNBig() big.Int {
 	return *NBig
 }
 
-func SecPSplit(num *big.Int) ([]*big.Int, error) {
+func SecPSplit(num *big.Int) ([]big.Int, error) {
 	// https://github.com/starkware-libs/cairo-lang/blob/efa9648f57568aad8f8a13fbf027d2de7c63c2c0/src/starkware/cairo/common/cairo_secp/secp_utils.py#L14
 
-	split := make([]*big.Int, 3)
+	split := make([]big.Int, 3)
 
 	baseBig, ok := getBaseBig()
 	if !ok {
@@ -60,8 +62,9 @@ func SecPSplit(num *big.Int) ([]*big.Int, error) {
 
 	var residue big.Int
 	for i := 0; i < 3; i++ {
-		num.DivMod(num, baseBig, &residue)
-		split[i] = new(big.Int).Set(&residue)
+		num.DivMod(num, &baseBig, &residue)
+		splitVal := new(big.Int).Set(&residue)
+		split[i] = *splitVal
 	}
 
 	if num.Cmp(big.NewInt(0)) != 0 {
@@ -73,6 +76,6 @@ func SecPSplit(num *big.Int) ([]*big.Int, error) {
 
 func GetSecp256R1_P() (big.Int, bool) {
 	// 2**256 - 2**224 + 2**192 + 2**96 - 1
-	secp256r1_p, ok := new(big.Int).SetString("115792089210356248762697446949407573530086143415290314195533631308867097853951", 10)
-	return *secp256r1_p, ok
+	secp256R1_P, ok := new(big.Int).SetString("115792089210356248762697446949407573530086143415290314195533631308867097853951", 10)
+	return *secp256R1_P, ok
 }
