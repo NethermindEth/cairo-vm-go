ada521fa521d55087224fd6c3660f0d09228f5ff
danielcdz <calde0601@gmai.com>
2024-04-26 07:52:44 -0600

    fix linter issues

--- pkg/hintrunner/zero/zerohint_ec.go
+++ pkg/hintrunner/zero/zerohint_ec.go
@@ -468,12 +468,11 @@ func newEcDoubleAssignNewXV1Hint(slope, point hinter.ResOperander) hinter.Hinter
 				return fmt.Errorf("GetSecPBig failed")
 			}
 
-			
 			multRes := new(big.Int)
 			multRes.Mul(big.NewInt(2), &xBig)
 
 			new_xBig := new(big.Int)
-			new_xBig.Exp(&slopeBig, big.NewInt(2), &secPBig)	
+			new_xBig.Exp(&slopeBig, big.NewInt(2), &secPBig)
 			new_xBig.Sub(new_xBig, multRes)
 			new_xBig.Mod(new_xBig, &secPBig)
 
