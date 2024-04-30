8a42ba8db10f7d367b46cc7331a831faae4517ee
Tuan Tran <tropicaldog17@gmail.com>
2024-04-30 22:37:33 +0700

    add test for assert ids_data

--- pkg/hintrunner/zero/zerohint_blake_test.go
+++ pkg/hintrunner/zero/zerohint_blake_test.go
@@ -494,5 +494,31 @@ func TestZeroHintBlake(t *testing.T) {
 					}),
 			},
 		},
+		"Blake2sFinalize check assert N_PACKED_INSTANCES": {
+			{
+				operanders: []*hintOperander{
+					{Name: "blake2s_ptr_end", Kind: apRelative, Value: addrWithSegment(1, 7)},
+					{Name: "N_PACKED_INSTANCES", Kind: fpRelative, Value: feltUint64(20)},
+					{Name: "INPUT_BLOCK_FELTS", Kind: apRelative, Value: feltUint64(16)},
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newBlake2sFinalizeHint(ctx.operanders["blake2s_ptr_end"], ctx.operanders["N_PACKED_INSTANCES"], ctx.operanders["INPUT_BLOCK_FELTS"])
+				},
+				errCheck: errorTextContains("in range [0, 20), got 20"),
+			},
+		},
+		"Blake2sFinalize check assert INPUT_BLOCK_FELTS": {
+			{
+				operanders: []*hintOperander{
+					{Name: "blake2s_ptr_end", Kind: apRelative, Value: addrWithSegment(1, 7)},
+					{Name: "N_PACKED_INSTANCES", Kind: fpRelative, Value: feltUint64(7)},
+					{Name: "INPUT_BLOCK_FELTS", Kind: apRelative, Value: feltUint64(200)},
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newBlake2sFinalizeHint(ctx.operanders["blake2s_ptr_end"], ctx.operanders["N_PACKED_INSTANCES"], ctx.operanders["INPUT_BLOCK_FELTS"])
+				},
+				errCheck: errorTextContains("in range [0, 100), got 200"),
+			},
+		},
 	})
 }
