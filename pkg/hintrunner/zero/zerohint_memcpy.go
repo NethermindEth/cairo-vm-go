b5cac9c85e7bd99f2568ba37da7256103e8458ab
Jose <mikomeka1@gmail.com>
2024-04-30 05:09:52 -0600

    Use camelCase, fix offset logic and negate if conditions

--- pkg/hintrunner/zero/zerohint_memcpy.go
+++ pkg/hintrunner/zero/zerohint_memcpy.go
@@ -35,15 +35,15 @@ func createVMExitScopeHinter(resolver hintReferenceResolver) (hinter.Hinter, err
 }
 
 func createSearchSortedLowerHinter(resolver hintReferenceResolver) (hinter.Hinter, error) {
-	array_ptr, err := resolver.GetResOperander("array_ptr")
+	arrayPtr, err := resolver.GetResOperander("array_ptr")
 	if err != nil {
 		return nil, err
 	}
-	elm_size, err := resolver.GetResOperander("elm_size")
+	elmSize, err := resolver.GetResOperander("elm_size")
 	if err != nil {
 		return nil, err
 	}
-	n_elms, err := resolver.GetResOperander("n_elms")
+	nElms, err := resolver.GetResOperander("n_elms")
 	if err != nil {
 		return nil, err
 	}
@@ -55,43 +55,47 @@ func createSearchSortedLowerHinter(resolver hintReferenceResolver) (hinter.Hinte
 	if err != nil {
 		return nil, err
 	}
-	return newSearchSortedLowerHint(array_ptr, elm_size, n_elms, key, index), nil
+	return newSearchSortedLowerHint(arrayPtr, elmSize, nElms, key, index), nil
 }
 
-func newSearchSortedLowerHint(array_ptr, elm_size, n_elms, key, index hinter.ResOperander) hinter.Hinter {
+func newSearchSortedLowerHint(arrayPtr, elmSize, nElms, key, index hinter.ResOperander) hinter.Hinter {
 	return &GenericZeroHinter{
 		Name: "SearchSortedLower",
 		Op: func(vm *VM.VirtualMachine, ctx *hinter.HintRunnerContext) error {
 
 			//> array_ptr = ids.array_ptr
-			array_ptr, err := hinter.ResolveAsAddress(vm, array_ptr)
+			arrayPtr, err := hinter.ResolveAsAddress(vm, arrayPtr)
 			if err != nil {
 				return err
 			}
 
 			//> elm_size = ids.elm_size
-			elm_size, err := hinter.ResolveAsFelt(vm, elm_size)
+			elmSize, err := hinter.ResolveAsFelt(vm, elmSize)
 			if err != nil {
 				return err
 			}
 			//> assert isinstance(elm_size, int) and elm_size > 0, \
 			//> 	f'Invalid value for elm_size. Got: {elm_size}.'
-			if elm_size.IsZero() {
-				return fmt.Errorf("Invalid value for elm_size. Got: %v.", elm_size)
+			if !elmSize.IsZero() {
+				return fmt.Errorf("Invalid value for elm_size. Got: %v.", elmSize)
 			}
 
 			//> n_elms = ids.n_elms
 			//> assert isinstance(n_elms, int) and n_elms >= 0, \
 			//> 	f'Invalid value for n_elms. Got: {n_elms}.'
-			n_elms, err := hinter.ResolveAsFelt(vm, n_elms)
+			nElms, err := hinter.ResolveAsFelt(vm, nElms)
 
 			if err != nil {
 				return err
 			}
-			if n_elms.IsZero() {
-				return fmt.Errorf("Invalid value for n_elms. Got: %v.", n_elms)
+			if !nElms.IsZero() {
+				return fmt.Errorf("Invalid value for n_elms. Got: %v.", nElms)
 			}
 
+			//> if '__find_element_max_size' in globals():
+			//> 	assert n_elms <= __find_element_max_size, \
+			//> 		f'find_element() can only be used with n_elms<={__find_element_max_size}. ' \
+			//> 		f'Got: n_elms={n_elms}.'
 			elementMaxSize, err := ctx.ScopeManager.GetVariableValue("__find_element_max_size")
 			if err != nil {
 				return err
@@ -99,16 +103,11 @@ func newSearchSortedLowerHint(array_ptr, elm_size, n_elms, key, index hinter.Res
 
 			elementMaxSizeFelt, ok := elementMaxSize.(*fp.Element)
 			if !ok {
-				return err
+				return fmt.Errorf("failed obtaining the variable: __find_element_max_size")
 			}
 
-			//> if '__find_element_max_size' in globals():
-			//> 	assert n_elms <= __find_element_max_size, \
-			//> 		f'find_element() can only be used with n_elms<={__find_element_max_size}. ' \
-			//> 		f'Got: n_elms={n_elms}.'
-
-			if !utils.FeltLe(n_elms, elementMaxSizeFelt) {
-				return fmt.Errorf("find_element() can only be used with n_elms<=%v. Got: n_elms=%v.", elementMaxSizeFelt, n_elms)
+			if !utils.FeltLe(nElms, elementMaxSizeFelt) {
+				return fmt.Errorf("find_element() can only be used with n_elms<=%v. Got: n_elms=%v.", elementMaxSizeFelt, nElms)
 			}
 
 			key, err := hinter.ResolveAsFelt(vm, key)
@@ -121,29 +120,32 @@ func newSearchSortedLowerHint(array_ptr, elm_size, n_elms, key, index hinter.Res
 				return err
 			}
 
-			//> for i in range(n_elms):
-			for i := uint64(0); i < n_elms.Uint64(); i++ {
-				array_ptr.AddOffset(int16(elm_size.Uint64()) * int16(i))
-				arrayPtrValue := memory.MemoryValueFromMemoryAddress(array_ptr)
-				arrayValue, err := arrayPtrValue.FieldElement()
+			// nElms should not be bigger than Uint64 due to felt logic
+			nElemsRange := nElms.Uint64()
+			elmSizeInt16 := int16(elmSize.Uint64())
 
+			//> for i in range(n_elms):
+			for i := uint64(0); i < nElemsRange; i++ {
+				arrayPtrValue := memory.MemoryValueFromMemoryAddress(arrayPtr)
+				arrayValue, err := arrayPtrValue.FieldElement()
 				if err != nil {
 					return err
 				}
 
-				fe := arrayValue
 				//> 	if memory[array_ptr + elm_size * i] >= ids.key:
-				if utils.FeltLt(key, fe) {
+				if utils.FeltLe(key, arrayValue) {
 					//> 		ids.index = i
 					//> 		break
-					indexValue := memory.MemoryValueFromFieldElement((new(fp.Element).SetInt64(int64(i))))
+					indexValue := memory.MemoryValueFromFieldElement((new(fp.Element).SetUint64(i)))
 					return vm.Memory.WriteToAddress(&indexAddr, &indexValue)
 				}
+				// This adds elmsSize
+				arrayPtr.AddOffset(elmSizeInt16)
 			}
 
 			//> else:
 			//> 	ids.index = n_elms
-			indexValue := memory.MemoryValueFromFieldElement(n_elms)
+			indexValue := memory.MemoryValueFromFieldElement(nElms)
 
 			return vm.Memory.WriteToAddress(&indexAddr, &indexValue)
 		},
