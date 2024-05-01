d93ad9ce4fb0eaeceb513501affc405409913014
Daniel Kyutae Jung <45947071+pxyxyrus@users.noreply.github.com>
2024-05-01 05:33:21 -0700

    Issue/233 usort verify (#358)
    * usort verify hint implementation
    
    * UsortVerify test draft
    
    * fix buggy code
    
    * fix: positions_dict and positions to correct type
    
    * remove unused test code
    
    * add correct hint code as comment for each hint
    
    * refactor code
    
    ---------
    
    Co-authored-by: Carmen Irene Cabrera Rodríguez <49727740+cicr99@users.noreply.github.com>

--- pkg/hintrunner/zero/zerohint_usort.go
+++ pkg/hintrunner/zero/zerohint_usort.go
@@ -1,14 +1,20 @@
 package zero
 
 import (
+	"fmt"
+
 	"github.com/NethermindEth/cairo-vm-go/pkg/hintrunner/hinter"
+	"github.com/NethermindEth/cairo-vm-go/pkg/utils"
 	VM "github.com/NethermindEth/cairo-vm-go/pkg/vm"
+	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
 )
 
 func newUsortEnterScopeHint() hinter.Hinter {
 	return &GenericZeroHinter{
 		Name: "UsortEnterScope",
 		Op: func(vm *VM.VirtualMachine, ctx *hinter.HintRunnerContext) error {
+			//> vm_enter_scope(dict(__usort_max_size = globals().get('__usort_max_size')))
+
 			usortMaxSize, err := ctx.ScopeManager.GetVariableValue("__usort_max_size")
 			if err != nil {
 				return err
@@ -25,4 +31,50 @@ func newUsortEnterScopeHint() hinter.Hinter {
 
 func createUsortEnterScopeHinter() (hinter.Hinter, error) {
 	return newUsortEnterScopeHint(), nil
+}
+
+func newUsortVerifyHinter(value hinter.ResOperander) hinter.Hinter {
+	return &GenericZeroHinter{
+		Name: "UsortVerify",
+		Op: func(vm *VM.VirtualMachine, ctx *hinter.HintRunnerContext) error {
+			//> last_pos = 0
+			//> positions = positions_dict[ids.value][::-1]
+
+			positionsDictInterface, err := ctx.ScopeManager.GetVariableValue("positions_dict")
+
+			if err != nil {
+				return err
+			}
+
+			positionsDict, ok := positionsDictInterface.(map[fp.Element][]uint64)
+
+			if !ok {
+				return fmt.Errorf("casting positions_dict into an dictionary failed")
+			}
+
+			value, err := hinter.ResolveAsFelt(vm, value)
+
+			if err != nil {
+				return err
+			}
+
+			positions := positionsDict[*value]
+			utils.Reverse(positions)
+
+			return ctx.ScopeManager.AssignVariables(map[string]any{
+				"last_pos":  0,
+				"positions": positions,
+			})
+		},
+	}
+}
+
+func createUsortVerifyHinter(resolver hintReferenceResolver) (hinter.Hinter, error) {
+	value, err := resolver.GetResOperander("value")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return newUsortVerifyHinter(value), nil
 }
