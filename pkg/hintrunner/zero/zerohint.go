a1019f0e767899aa12cc51dbedbf94bb16822267
Daniel Calderón Díaz <66186331+danielcdz@users.noreply.github.com>
2024-05-01 08:01:56 -0600

    Implement EcDoubleAssignNewXV1 hint (#367)
    * add EcDoubleAssignNewXV1 hint
    
    * remove extra space
    
    * fix linter issues
    
    * simplify loops for point values filling
    
    * fix aritmethic operations order
    
    - Update unit test
    
    * fix function naming
    
    * update tests to match new function name
    
    * fix linter issues
    
    * check new_x value in tests
    
    ---------
    
    Co-authored-by: danielcdz <calde0601@gmai.com>

--- pkg/hintrunner/zero/zerohint.go
+++ pkg/hintrunner/zero/zerohint.go
@@ -131,6 +131,8 @@ func GetHintFromCode(program *zero.ZeroProgram, rawHint zero.Hint, hintPC uint64
 		return createFastEcAddAssignNewXHinter(resolver)
 	case ecDoubleSlopeV1Code:
 		return createEcDoubleSlopeV1Hinter(resolver)
+	case ecDoubleAssignNewXV1:
+		return createEcDoubleAssignNewXV1Hinter(resolver)
 		// Blake hints
 	case blake2sAddUint256BigendCode:
 		return createBlake2sAddUint256Hinter(resolver, true)
