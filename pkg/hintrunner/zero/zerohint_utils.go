4289e7b02a9946e7e367218b3c7c264fcbd4872a
TAdev0 <angles.tristan@protonmail.com>
2024-05-08 13:26:23 +0200

    address comments

--- pkg/hintrunner/zero/zerohint_utils.go
+++ pkg/hintrunner/zero/zerohint_utils.go
@@ -3,19 +3,42 @@ package zero
 import (
 	"github.com/NethermindEth/cairo-vm-go/pkg/hintrunner/hinter"
 	VM "github.com/NethermindEth/cairo-vm-go/pkg/vm"
+	mem "github.com/NethermindEth/cairo-vm-go/pkg/vm/memory"
 	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
 )
 
+// GetUint256AsFelts helper function reads two consecutive memory cells,
+// the first one containing the low part of the `uint256` and the second one
+// containing the high part of the `uint256`
+//
+// The low and high parts previously extracted from memory are then
+// converted to field elements and returned
 func GetUint256AsFelts(vm *VM.VirtualMachine, ref hinter.ResOperander) (*fp.Element, *fp.Element, error) {
-	refAddr, err := ref.GetAddress(vm)
+	lowRefAddr, err := ref.GetAddress(vm)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	values, err := hinter.GetConsecutiveValues(vm, refAddr, int16(2))
+	values := make([]mem.MemoryValue, 2)
+
+	lowPart, err := vm.Memory.ReadFromAddress(&lowRefAddr)
 	if err != nil {
 		return nil, nil, err
 	}
+
+	values[0] = lowPart
+
+	highRefAddr, err := lowRefAddr.AddOffset(1)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	highPart, err := vm.Memory.ReadFromAddress(&highRefAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	values[1] = highPart
 
 	low, err := values[0].FieldElement()
 	if err != nil {
