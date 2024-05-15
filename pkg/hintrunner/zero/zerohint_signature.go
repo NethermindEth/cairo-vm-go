25565738bfb1eeedf3fed28e7371bd360d75a37f
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-05-15 16:44:54 +0300

    refactor: move GetConsecutiveValues functionality to memory package (#396)
    * refactor: move GetConsecutiveValues functionality to memory package
    
    * refactor: rename GetConsecutiveValues func and cleanup reference to it
    
    * fix: fix unit tests bugs

--- pkg/hintrunner/zero/zerohint_signature.go
+++ pkg/hintrunner/zero/zerohint_signature.go
@@ -42,7 +42,7 @@ func newVerifyZeroHint(val, q hinter.ResOperander) hinter.Hinter {
 				return err
 			}
 
-			valMemoryValues, err := hinter.GetConsecutiveValues(vm, valAddr, int16(3))
+			valMemoryValues, err := vm.Memory.GetConsecutiveMemoryValues(valAddr, int16(3))
 			if err != nil {
 				return err
 			}
@@ -177,7 +177,7 @@ func newGetPointFromXHint(xCube, v hinter.ResOperander) hinter.Hinter {
 				return err
 			}
 
-			xCubeMemoryValues, err := hinter.GetConsecutiveValues(vm, xCubeAddr, 3)
+			xCubeMemoryValues, err := vm.Memory.GetConsecutiveMemoryValues(xCubeAddr, int16(3))
 			if err != nil {
 				return err
 			}
@@ -334,7 +334,7 @@ func newDivModNPackedDivmodV1Hint(a, b hinter.ResOperander) hinter.Hinter {
 			if err != nil {
 				return err
 			}
-			aMemoryValues, err := hinter.GetConsecutiveValues(vm, aAddr, int16(3))
+			aMemoryValues, err := vm.Memory.GetConsecutiveMemoryValues(aAddr, int16(3))
 			if err != nil {
 				return err
 			}
@@ -343,7 +343,7 @@ func newDivModNPackedDivmodV1Hint(a, b hinter.ResOperander) hinter.Hinter {
 			if err != nil {
 				return err
 			}
-			bMemoryValues, err := hinter.GetConsecutiveValues(vm, bAddr, int16(3))
+			bMemoryValues, err := vm.Memory.GetConsecutiveMemoryValues(bAddr, int16(3))
 			if err != nil {
 				return err
 			}
