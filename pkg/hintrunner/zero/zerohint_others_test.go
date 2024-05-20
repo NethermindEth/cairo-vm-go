0717dd410b7a11d4d5e97b9733a30a91aeaac085
Daniel Calderón Díaz <66186331+danielcdz@users.noreply.github.com>
2024-05-20 05:58:16 -0600

    Implement MemcpyContinueCopying hint (#359)
    * draft add MencpyContinueCopying hint
    
    * remove n and improve params naming
    
    * fix comments to match standard
    
    * fix comparision and v assignment
    
    * rename variable
    
    * fix lint issues
    
    * align string word
    
    * fix function name
    
    * improve if-else statement
    
    * improve hint go code
    
    * fix linter issues
    
    * invert FeltZero and FeltOne in condition
    
    * add unit testing
    
    * store new n value
    
    * rename variable to continueCopying
    
    * handle error when assigning variable n
    
    * rename variable v
    
    * use felt instead of uint64
    
    * modify tests and run linter
    
    * move code to zerohint_others
    
    * Simplify enter scope variable
    
    * rename variable
    
    ---------
    
    Co-authored-by: danielcdz <calde0601@gmai.com>
    Co-authored-by: Tristan <122918260+TAdev0@users.noreply.github.com>

--- pkg/hintrunner/zero/zerohint_others_test.go
+++ pkg/hintrunner/zero/zerohint_others_test.go
@@ -4,11 +4,48 @@ import (
 	"testing"
 
 	"github.com/NethermindEth/cairo-vm-go/pkg/hintrunner/hinter"
+	"github.com/NethermindEth/cairo-vm-go/pkg/utils"
 )
 
 func TestZeroHintMemcpy(t *testing.T) {
 
 	runHinterTests(t, map[string][]hintTestCase{
+		"MemcPyContinueCopying": {
+			{
+				operanders: []*hintOperander{
+					{Name: "continue_copying", Kind: uninitialized},
+				},
+				ctxInit: func(ctx *hinter.HintRunnerContext) {
+					ctx.ScopeManager.EnterScope(map[string]any{
+						"n": &utils.FeltOne,
+					})
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newMemcpyContinueCopyingHint(ctx.operanders["continue_copying"])
+				},
+				check: func(t *testing.T, ctx *hintTestContext) {
+					allVarValueInScopeEquals(map[string]any{"n": feltInt64(0)})(t, ctx)
+					varValueEquals("continue_copying", feltInt64(0))(t, ctx)
+				},
+			},
+			{
+				operanders: []*hintOperander{
+					{Name: "continue_copying", Kind: uninitialized},
+				},
+				ctxInit: func(ctx *hinter.HintRunnerContext) {
+					ctx.ScopeManager.EnterScope(map[string]any{
+						"n": feltString("5"),
+					})
+				},
+				makeHinter: func(ctx *hintTestContext) hinter.Hinter {
+					return newMemcpyContinueCopyingHint(ctx.operanders["continue_copying"])
+				},
+				check: func(t *testing.T, ctx *hintTestContext) {
+					allVarValueInScopeEquals(map[string]any{"n": feltInt64(4)})(t, ctx)
+					varValueEquals("continue_copying", feltInt64(1))(t, ctx)
+				},
+			},
+		},
 		"MemcpyEnterScope": {
 			{
 				operanders: []*hintOperander{
