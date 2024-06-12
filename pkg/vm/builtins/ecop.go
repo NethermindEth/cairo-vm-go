d31bcd91230727b7901213c849e317f557980e55
MaksymMalicki <maksym.malicki.2115@gmail.com>
2024-06-12 18:27:36 +0200

    Lint the project

--- pkg/vm/builtins/ecop.go
+++ pkg/vm/builtins/ecop.go
@@ -24,8 +24,7 @@ var feltThree f.Element = f.Element(
 	})
 
 type EcOp struct {
-	ratio                     uint64
-	instancesPerComponentEcOp uint64
+	ratio uint64
 }
 
 func (e *EcOp) String() string {
@@ -107,7 +106,7 @@ func (e *EcOp) InferValue(segment *mem.Segment, offset uint64) error {
 }
 
 func (e *EcOp) GetAllocatedSize(segmentUsedSize uint64, vmCurrentStep uint64) (uint64, error) {
-	allocatedInstances, err := GetAllocatedInstances(e.ratio, inputCellsPerEcOp, segmentUsedSize, e.instancesPerComponentEcOp, vmCurrentStep)
+	allocatedInstances, err := GetAllocatedInstances(e.ratio, inputCellsPerEcOp, segmentUsedSize, instancesPerComponentEcOp, vmCurrentStep)
 	if err != nil {
 		return 0, err
 	}
