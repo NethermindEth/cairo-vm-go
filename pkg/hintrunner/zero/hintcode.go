79a04233249261bec0f4f61769e9184800b636f8
TAdev0 <angles.tristan@protonmail.com>
2024-06-13 00:43:42 +0200

    fmt

--- pkg/hintrunner/zero/hintcode.go
+++ pkg/hintrunner/zero/hintcode.go
@@ -35,8 +35,8 @@ const (
 	splitIntCode            string = "memory[ids.output] = res = (int(ids.value) % PRIME) % ids.base\nassert res < ids.bound, f'split_int(): Limb {res} is out of range.'"
 
 	// pow() hint
-	powCode string = "ids.locs.bit = (ids.prev_locs.exp % PRIME) & 1"
-  signedPowCode string = "assert ids.base != 0, 'Cannot raise 0 to a negative power.'"
+	powCode       string = "ids.locs.bit = (ids.prev_locs.exp % PRIME) & 1"
+	signedPowCode string = "assert ids.base != 0, 'Cannot raise 0 to a negative power.'"
 
 	// div_rem() hints
 	signedDivRemCode   string = "from starkware.cairo.common.math_utils import as_int, assert_integer\n\nassert_integer(ids.div)\nassert 0 < ids.div <= PRIME // range_check_builtin.bound, \\\n    f'div={hex(ids.div)} is out of the valid range.'\n\nassert_integer(ids.bound)\nassert ids.bound <= range_check_builtin.bound // 2, \\\n    f'bound={hex(ids.bound)} is out of the valid range.'\n\nint_value = as_int(ids.value, PRIME)\nq, ids.r = divmod(int_value, ids.div)\n\nassert -ids.bound <= q < ids.bound, \\\n    f'{int_value} / {ids.div} = {q} is out of the range [{-ids.bound}, {ids.bound}).'\n\nids.biased_q = q + ids.bound"
