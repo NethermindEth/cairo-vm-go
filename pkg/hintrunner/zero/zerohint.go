034e0bcbd846692e937835c22e518c26f3495331
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-06-19 19:20:39 +0530

    Fix merge

--- pkg/hintrunner/zero/zerohint.go
+++ pkg/hintrunner/zero/zerohint.go
@@ -122,6 +122,8 @@ func GetHintFromCode(program *zero.ZeroProgram, rawHint zero.Hint, hintPC uint64
 		return createVerifyZeroHinter(resolver)
 	case divModNPackedDivmodV1Code:
 		return createDivModNPackedDivmodV1Hinter(resolver)
+	case isZeroPackCode:
+		return createIsZeroPackHinter(resolver)
 	// EC hints
 	case ecNegateCode:
 		return createEcNegateHinter(resolver)
