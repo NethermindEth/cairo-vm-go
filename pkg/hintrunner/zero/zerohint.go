27817ff6ccf8f8fef2b26be3b72ece39fb0cef21
Shourya Goel <shouryagoel10000@gmail.com>
2024-07-03 18:09:49 +0530

    Implemented `SplitOutputMidLowHigh` (#511)
    Implemented SplitOutputMidLowHigh

--- pkg/hintrunner/zero/zerohint.go
+++ pkg/hintrunner/zero/zerohint.go
@@ -173,6 +173,8 @@ func GetHintFromCode(program *zero.ZeroProgram, rawHint zero.Hint, hintPC uint64
 		return createBlockPermutationHinter(resolver)
 	case compareBytesInWordCode:
 		return createCompareBytesInWordNondetHinter(resolver)
+	case splitOutputMidLowHighCode:
+		return createSplitOutputMidLowHighHinter(resolver)
 	// Usort hints
 	case usortEnterScopeCode:
 		return createUsortEnterScopeHinter()
