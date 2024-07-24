b26fe18ca3071e8ae24b496d4b1eecd4b4dde395
Harikrishnan Shaji <hihari777@gmail.com>
2024-07-24 17:11:29 +0530

    Fix dict implementation (#575)
    * Update implementation with MemoryValue
    
    * Fix more resolves

--- pkg/hintrunner/hinter/zero_dict.go
+++ pkg/hintrunner/hinter/zero_dict.go
@@ -5,13 +5,12 @@ import (
 
 	VM "github.com/NethermindEth/cairo-vm-go/pkg/vm"
 	mem "github.com/NethermindEth/cairo-vm-go/pkg/vm/memory"
-	"github.com/consensys/gnark-crypto/ecc/stark-curve/fp"
 )
 
 // Used to keep track of all Dictionaries data
 type ZeroDictionary struct {
 	// The Data contained in a dictionary
-	Data *map[fp.Element]mem.MemoryValue
+	Data *map[mem.MemoryValue]mem.MemoryValue
 	// Default value for key not present in the dictionary
 	DefaultValue *mem.MemoryValue
 	// first free offset in memory segment of dictionary
@@ -19,7 +18,7 @@ type ZeroDictionary struct {
 }
 
 // Gets the memory value at certain key
-func (d *ZeroDictionary) at(key fp.Element) (mem.MemoryValue, error) {
+func (d *ZeroDictionary) at(key mem.MemoryValue) (mem.MemoryValue, error) {
 	if value, ok := (*d.Data)[key]; ok {
 		return value, nil
 	}
@@ -30,7 +29,7 @@ func (d *ZeroDictionary) at(key fp.Element) (mem.MemoryValue, error) {
 }
 
 // Given a key and a value, it sets the value at the given key
-func (d *ZeroDictionary) set(key fp.Element, value mem.MemoryValue) {
+func (d *ZeroDictionary) set(key mem.MemoryValue, value mem.MemoryValue) {
 	(*d.Data)[key] = value
 }
 
@@ -59,7 +58,7 @@ func NewZeroDictionaryManager() ZeroDictionaryManager {
 // It creates a new segment which will hold dictionary values. It links this
 // segment with the current dictionary and returns the address that points
 // to the start of this segment. initial dictionary data is set from the data argument.
-func (dm *ZeroDictionaryManager) NewDictionary(vm *VM.VirtualMachine, data map[fp.Element]mem.MemoryValue) mem.MemoryAddress {
+func (dm *ZeroDictionaryManager) NewDictionary(vm *VM.VirtualMachine, data map[mem.MemoryValue]mem.MemoryValue) mem.MemoryAddress {
 	newDictAddr := vm.Memory.AllocateEmptySegment()
 	freeOffset := uint64(0)
 	dm.Dictionaries[newDictAddr.SegmentIndex] = ZeroDictionary{
@@ -76,7 +75,7 @@ func (dm *ZeroDictionaryManager) NewDictionary(vm *VM.VirtualMachine, data map[f
 // querying the defaultValue will be returned instead.
 func (dm *ZeroDictionaryManager) NewDefaultDictionary(vm *VM.VirtualMachine, defaultValue mem.MemoryValue) mem.MemoryAddress {
 	newDefaultDictAddr := vm.Memory.AllocateEmptySegment()
-	newData := make(map[fp.Element]mem.MemoryValue)
+	newData := make(map[mem.MemoryValue]mem.MemoryValue)
 	freeOffset := uint64(0)
 	dm.Dictionaries[newDefaultDictAddr.SegmentIndex] = ZeroDictionary{
 		Data:         &newData,
@@ -101,7 +100,7 @@ func (dm *ZeroDictionaryManager) GetDictionary(dictAddr mem.MemoryAddress) (Zero
 
 // Given a memory address and a key it returns the value held at that position. The address is used
 // to locate the correct dictionary and the key to index on it
-func (dm *ZeroDictionaryManager) At(dictAddr mem.MemoryAddress, key fp.Element) (mem.MemoryValue, error) {
+func (dm *ZeroDictionaryManager) At(dictAddr mem.MemoryAddress, key mem.MemoryValue) (mem.MemoryValue, error) {
 	dict, err := dm.GetDictionary(dictAddr)
 	if err != nil {
 		return mem.UnknownValue, err
@@ -114,7 +113,7 @@ func (dm *ZeroDictionaryManager) At(dictAddr mem.MemoryAddress, key fp.Element) 
 }
 
 // Given a memory address,a key and a value it stores the value at the correct position.
-func (dm *ZeroDictionaryManager) Set(dictAddr mem.MemoryAddress, key fp.Element, value mem.MemoryValue) error {
+func (dm *ZeroDictionaryManager) Set(dictAddr mem.MemoryAddress, key mem.MemoryValue, value mem.MemoryValue) error {
 	dict, err := dm.GetDictionary(dictAddr)
 	if err != nil {
 		return err
