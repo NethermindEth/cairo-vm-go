72539dbcbb52699ca47143f6dcc5b7a2e55ecfb6
TAdev0 <angles.tristan@protonmail.com>
2024-07-29 18:02:24 +0200

    fmt

--- pkg/hintrunner/zero/zerohint.go
+++ pkg/hintrunner/zero/zerohint.go
@@ -215,10 +215,10 @@ func GetHintFromCode(program *zero.ZeroProgram, rawHint zero.Hint) (hinter.Hinte
 	// Sha256 hints
 	case packedSha256Code:
 		return createPackedSha256Hinter(resolver)
+	case sha256ChunkCode:
+		return createSha256ChunkHinter(resolver)
 	case finalizeSha256Code:
 		return createFinalizeSha256Hinter(resolver)
-   	case sha256ChunkCode:
-		return createSha256ChunkHinter(resolver)
 	// Keccak hints
 	case keccakWriteArgsCode:
 		return createKeccakWriteArgsHinter(resolver)
