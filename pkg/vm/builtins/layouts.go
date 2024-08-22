42745e9dac982f5826cff8b339e99d739d4892ec
Shourya Goel <shouryagoel10000@gmail.com>
2024-08-22 19:24:33 +0530

    Update layouts.go

--- pkg/vm/builtins/layouts.go
+++ pkg/vm/builtins/layouts.go
@@ -54,13 +54,13 @@ func getAllCairoLayout() Layout {
 	return Layout{Name: "all_cairo", RcUnits: 8, Builtins: []LayoutBuiltin{
 		{Runner: &Output{}, Builtin: starknet.Output},
 		{Runner: &Pedersen{ratio: 256}, Builtin: starknet.Pedersen},
-		{Runner: &RangeCheck{ratio: 8, RANGE_CHECK_N_PARTS: 8}, Builtin: starknet.RangeCheck},
+		{Runner: &RangeCheck{ratio: 8, RangeCheckNParts: 8}, Builtin: starknet.RangeCheck},
 		{Runner: &ECDSA{ratio: 2048}, Builtin: starknet.ECDSA},
 		{Runner: &Bitwise{ratio: 16}, Builtin: starknet.Bitwise},
 		{Runner: &EcOp{ratio: 1024, cache: make(map[uint64]fp.Element)}, Builtin: starknet.ECOP},
 		{Runner: &Keccak{ratio: 2048, cache: make(map[uint64]fp.Element)}, Builtin: starknet.Keccak},
 		{Runner: &Poseidon{ratio: 256, cache: make(map[uint64]fp.Element)}, Builtin: starknet.Poseidon},
-		{Runner: &RangeCheck{ratio: 8, RANGE_CHECK_N_PARTS: 6}, Builtin: starknet.RangeCheck96},
+		{Runner: &RangeCheck{ratio: 8, RangeCheckNParts: 6}, Builtin: starknet.RangeCheck96},
 	}}
 }
 
