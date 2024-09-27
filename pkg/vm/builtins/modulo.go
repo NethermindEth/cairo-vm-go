b86a6371a136d1bb89ec3531fc9666b998b27053
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-09-27 13:40:06 +0530

    Added mirror functionality

--- pkg/vm/builtins/modulo.go
+++ pkg/vm/builtins/modulo.go
@@ -390,7 +390,24 @@ func (m *ModBuiltin) fillValue(mem *memory.Memory, inputs ModBuiltinInputs, inde
 				}
 			}
 		} else {
-			value = *new(big.Int).Div(c, a)
+			x, _, gcd := utils.Igcdex(a, &inputs.p)
+			// if gcd != 1, the known value is 0, in which case the res must be 0
+			if gcd.Cmp(big.NewInt(1)) != 0 {
+				value = *new(big.Int).Div(&inputs.p, &gcd)
+			} else {
+				value = *new(big.Int).Mul(c, &x)
+				value = *value.Mod(&value, &inputs.p)
+				tmpK, err := utils.SafeDiv(new(big.Int).Sub(new(big.Int).Mul(a, &value), c), &inputs.p)
+				if err != nil {
+					return false, err
+				}
+				if tmpK.Cmp(kBound) >= 0 {
+					return false, fmt.Errorf("%s builtin: ((%d * q) - %d) / %d > %d for any q > 0, such that %d * q = %d (mod %d) ", m.String(), a, c, &inputs.p, kBound, a, c, &inputs.p)
+				}
+				if tmpK.Cmp(big.NewInt(0)) < 0 {
+					value = *value.Add(&value, new(big.Int).Mul(&inputs.p, new(big.Int).Div(new(big.Int).Sub(a, new(big.Int).Sub(&tmpK, big.NewInt(1))), a)))
+				}
+			}
 		}
 		if err := m.writeNWordsValue(mem, addresses[1], value); err != nil {
 			return false, err
